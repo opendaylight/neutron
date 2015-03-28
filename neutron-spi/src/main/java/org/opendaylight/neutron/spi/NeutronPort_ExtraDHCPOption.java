0831c3e5f037bf19cdb677baa871726900f0ed25
Ryan Moats <rmoats@us.ibm.com>
2015-03-28 07:16:10 -0500

    Update Neutron Port attributes to match OS Kilo model
    Change-Id: Iceb8e1a9f596901bfc0a82b2f65dc7d9dbda0280
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/NeutronPort_ExtraDHCPOption.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright IBM Corporation, 2015.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public class NeutronPort_ExtraDHCPOption implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @XmlElement (name = "opt_value")
+    String value;
+
+    @XmlElement (name = "opt_name")
+    String name;
+
+    public NeutronPort_ExtraDHCPOption() {
+    }
+
+    public NeutronPort_ExtraDHCPOption(String value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public String getValue() { return(value); }
+
+    public void setValue(String value) { this.value = value; }
+
+    public String getName() { return(name); }
+
+    public void setName(String name) { this.name = name; }
+
+}
