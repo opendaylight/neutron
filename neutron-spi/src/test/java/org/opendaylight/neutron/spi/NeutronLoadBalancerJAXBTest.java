951f1b56c548c63d679fc92a08974e476a0a8bed
shreshthajoshi <shreshtha.joshi@tcs.com>
2015-03-31 16:12:16 +0530

    Add Junit testing of JAXB for NeutronLoadBalancer and NeutronLoadBalancer_SessionPersistenceJAXB
    Change-Id: I23b4234cfec9b37ec50fb5b4572a4d9d5ad180eb
    Signed-off-by: shreshthajoshi <shreshtha.joshi@tcs.com>

---
+++ neutron-spi/src/test/java/org/opendaylight/neutron/spi/NeutronLoadBalancerJAXBTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright IBM Corporation and others, 2015.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import org.opendaylight.neutron.spi.NeutronLoadBalancer;
+
+public class NeutronLoadBalancerJAXBTest {
+
+    private static final String NeutronLoadBalancer_sourceJson = "{"
+            + "\"id\": \"2f245a7b-796b-4f26-9cf9-9e82d248fda7\", " + "\"name\": \"NeutronLoadBalancer\", "
+            + "\"description\": \"NeutronLoadBalancer_Description\", " + "\"status\": \"ACTIVE\", "
+            + "\"admin_state_up\": \"false\", " + "\"vip_address\": \"10.0.0.3\", "
+            + "\"vip_subnet_id\": \"d23abc8d-2991-4a55-ba98-2aaea84cc72f\", "
+            + "\"tenant_id\": \"4969c491a3c74ee4af974e6d800c62de\" }";
+
+    @Test
+    public void test_NeutronLoadBalancer_JAXB() {
+        NeutronLoadBalancer dummyObject = new NeutronLoadBalancer();
+        try {
+            NeutronLoadBalancer testObject = (NeutronLoadBalancer) JaxbTestHelper.jaxbUnmarshall(dummyObject,
+                    NeutronLoadBalancer_sourceJson);
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 1: Testing id failed",
+                    "2f245a7b-796b-4f26-9cf9-9e82d248fda7", testObject.loadBalancerID);
+
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 4: Testing LoadBalancer Name failed",
+                    "NeutronLoadBalancer", testObject.getLoadBalancerName());
+
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 5: Testing LoadBalancer Description failed",
+                    "NeutronLoadBalancer_Description", testObject.getLoadBalancerDescription());
+
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 7: Testing status failed", "ACTIVE",
+                    testObject.getLoadBalancerStatus());
+
+            Assert.assertFalse("NeutronLoadBalancer JAXB Test 7: Testing Admin state up failed",
+                    testObject.getLoadBalancerAdminStateUp());
+
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 6: Testing LoadBalancer VipAddress failed", "10.0.0.3",
+                    testObject.getLoadBalancerVipAddress());
+
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 3: Testing router id failed",
+                    "d23abc8d-2991-4a55-ba98-2aaea84cc72f", testObject.loadBalancerVipSubnetID);
+
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 2: Testing tenant_id failed",
+                    "4969c491a3c74ee4af974e6d800c62de", testObject.getLoadBalancerTenantID());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ neutron-spi/src/test/java/org/opendaylight/neutron/spi/NeutronLoadBalancer_SessionPersistenceJAXBTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright IBM Corporation and others, 2015.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NeutronLoadBalancer_SessionPersistenceJAXBTest {
+
+    private static final String NeutronLoadBalancer_SessionPersistence_sourceJson = "{"
+            + "\"cookie_name\": \"NeutronLoadBalancer_SessionPersistence_Cookie\", " + "\"type\": \"HTTP_COOKIE\"}";
+
+    @Test
+    public void test_NeutronLoadBalancer_SessionPersistence_JAXB() {
+        NeutronLoadBalancer_SessionPersistence dummyObject = new NeutronLoadBalancer_SessionPersistence();
+        try {
+            NeutronLoadBalancer_SessionPersistence testObject = (NeutronLoadBalancer_SessionPersistence) JaxbTestHelper
+                    .jaxbUnmarshall(dummyObject, NeutronLoadBalancer_SessionPersistence_sourceJson);
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 1: Testing id failed",
+                    "NeutronLoadBalancer_SessionPersistence_Cookie", testObject.getCookieName());
+
+            Assert.assertEquals("NeutronLoadBalancer JAXB Test 4: Testing LoadBalancer Name failed", "HTTP_COOKIE",
+                    testObject.getType());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
