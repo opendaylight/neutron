5e4a45c74e7cdbae1b02ece07b779015e94ef31e
bhavesh.kumar1 <bhavesh.kumar1@tcs.com>
2015-04-02 18:12:49 +0530

    Comments incorporated in NeutronRouter_NetworkReference
    Made changes as per the comment received, added enable_snat and
    external_fixed_ip elements in NeutronRouter_NetworkReferenceJAXBTest.java.
    
    Change-Id: If1a34891f42cc2b3e4cba9a73362d028db3bfa0f
    Signed-off-by: bhavesh.kumar1 <bhavesh.kumar1@tcs.com>

---
+++ neutron-spi/src/test/java/org/opendaylight/neutron/spi/NeutronRouter_NetworkReferenceJAXBTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015 IBM, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NeutronRouter_NetworkReferenceJAXBTest {
+
+    private static final String NeutronRouter_NetworkReference_sourceJson = "{" +
+        "\"network_id\": \"e9330b1f-a2ef-4160-a991-169e56ab17f6\" ," +
+        "\"enable_snat\": false , " +
+        "\"external_fixed_ips\": [ { \"ip_address\":\"192.168.111.3\" , \"subnet_id\": \"22b44fc2-4ffb-4de4-b0f9-69d58b37ae27\" } ]" + 
+        "}";
+
+    @Test
+    public void test_NeutronRouter_NetworkReference_JAXB() {
+        NeutronRouter_NetworkReference dummyObject = new NeutronRouter_NetworkReference();
+        try {
+            NeutronRouter_NetworkReference testObject = (NeutronRouter_NetworkReference) JaxbTestHelper.jaxbUnmarshall(
+                    dummyObject, NeutronRouter_NetworkReference_sourceJson);
+
+            List<Neutron_IPs> externalFixedIPs = testObject.getExternalFixedIPs();
+
+            Assert.assertEquals("NeutronRouter_NetworkReference JAXB Test 1: Testing network_id failed",
+                    "e9330b1f-a2ef-4160-a991-169e56ab17f6", testObject.getNetworkID());
+
+            Assert.assertEquals("NeutronRouter_NetworkReference JAXB Test 2: Testing enable_snat failed", false,
+                    testObject.getEnableSNAT());
+
+            Assert.assertEquals(
+                    "NeutronRouter_NetworkReference JAXB Test 3.1: Testing externalFixedIPs list length failed", 1,
+                    externalFixedIPs.size());
+
+            Assert.assertEquals("NeutronRouter_NetworkReference JAXB Test 3.2: Testing ip_address value failed",
+                    "192.168.111.3", externalFixedIPs.get(0).ipAddress);
+
+            Assert.assertEquals("NeutronRouter_NetworkReference JAXB Test 3.3: Testing subnet_id value failed",
+                    "22b44fc2-4ffb-4de4-b0f9-69d58b37ae27", externalFixedIPs.get(0).subnetUUID);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
