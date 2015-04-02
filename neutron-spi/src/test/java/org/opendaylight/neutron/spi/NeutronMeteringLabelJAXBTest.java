095f2fd84828a20871be83cff5a4297edbb1e001
shreshthajoshi <shreshtha.joshi@tcs.com>
2015-04-02 17:05:07 +0530

    JUnit JAXB tests for NeutronMeteringLabel, NeutronMeteringLabelRule and NeutronLoadBalancerPoolMember
    Change-Id: I3659b2626c0f7a99e81ad258c5711440f0acfd1f
    Signed-off-by: shreshthajoshi <shreshtha.joshi@tcs.com>

---
+++ neutron-spi/src/test/java/org/opendaylight/neutron/spi/NeutronMeteringLabelJAXBTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2015 IBM, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.opendaylight.neutron.spi.JaxbTestHelper;
+import org.opendaylight.neutron.spi.NeutronMeteringLabel;
+
+public class NeutronMeteringLabelJAXBTest {
+
+    private static final String NeutronMeteringLabel_sourceJson = "{ "
+            + "\"id\": \"4e8e5957-649f-477b-9e5b-f1f75b21c03c\", " + "\"name\": \"net1\", "
+            + "\"description\": \"Provides allowed address pairs\", "
+            + "\"tenant_id\": \"9bacb3c5d39d41a79512987f338cf177\"}";
+
+    @Test
+    public void test_NeutronMeteringLabel_JAXB() {
+        NeutronMeteringLabel meteringObject = new NeutronMeteringLabel();
+        try {
+            NeutronMeteringLabel testObject = (NeutronMeteringLabel) JaxbTestHelper.jaxbUnmarshall(meteringObject,
+                    NeutronMeteringLabel_sourceJson);
+            Assert.assertEquals("NeutronMeteringLabel JAXB Test 1: Testing id failed",
+                    "4e8e5957-649f-477b-9e5b-f1f75b21c03c", testObject.getMeteringLabelUUID());
+            
+            Assert.assertEquals("NeutronMeteringLabel JAXB Test 2: Testing name failed", "net1",
+                    testObject.getMeteringLabelName());
+
+            Assert.assertEquals("NeutronMeteringLabel JAXB Test 3: Testing description failed",
+                    "Provides allowed address pairs", testObject.getMeteringDescription());
+
+            Assert.assertEquals("NeutronMeteringLabel JAXB Test 4: Testing tenant_id failed",
+                    "9bacb3c5d39d41a79512987f338cf177", testObject.getMeteringTenantID());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
