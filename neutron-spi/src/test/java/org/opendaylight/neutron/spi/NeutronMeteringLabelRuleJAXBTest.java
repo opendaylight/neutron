dfe1d22a79c12c717d36901c240fc8a3bd38b6fe
shreshthajoshi <shreshtha.joshi@tcs.com>
2015-04-02 17:05:07 +0530

    JUnit JAXB tests for NeutronMeteringLabel, NeutronMeteringLabelRule and NeutronLoadBalancerPoolMember
    Patch set 2: changed a few nits
    
    Change-Id: I3659b2626c0f7a99e81ad258c5711440f0acfd1f
    Signed-off-by: shreshthajoshi <shreshtha.joshi@tcs.com>

---
+++ neutron-spi/src/test/java/org/opendaylight/neutron/spi/NeutronMeteringLabelRuleJAXBTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015 IBM, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.opendaylight.neutron.spi.JaxbTestHelper;
+import org.opendaylight.neutron.spi.NeutronMeteringLabelRule;
+
+public class NeutronMeteringLabelRuleJAXBTest {
+
+    private static final String NeutronMeteringLabelRule_sourceJson = "{ "
+            + "\"metering_label_id\": \"e131d186-b02d-4c0b-83d5-0c0725c4f812\", "
+            + "\"remote_ip_prefix\": \"10.0.0.0/24\", " + "\"direction\": \"ingress\", " + "\"excluded\": false , "
+            + "\"id\": \"4e8e5957-649f-477b-9e5b-f1f75b21c03c\" }";
+
+    @Test
+    public void test_NeutronMeteringLabelRule_JAXB() {
+        NeutronMeteringLabelRule meteringObject = new NeutronMeteringLabelRule();
+        try {
+            NeutronMeteringLabelRule testObject = (NeutronMeteringLabelRule) JaxbTestHelper.jaxbUnmarshall(
+                    meteringObject, NeutronMeteringLabelRule_sourceJson);
+            Assert.assertEquals("NeutronMeteringLabelRule JAXB Test 1: Testing metering_label_id failed",
+                    "e131d186-b02d-4c0b-83d5-0c0725c4f812", testObject.getMeteringLabelRuleLabelID());
+
+            Assert.assertEquals("NeutronMeteringLabelRule JAXB Test 2: Testing remote_ip_prefix failed", "10.0.0.0/24",
+                    testObject.getMeteringLabelRuleRemoteIPPrefix());
+
+            Assert.assertEquals("NeutronMeteringLabelRule JAXB Test 3: Testing direction failed", "ingress",
+                    testObject.getMeteringLabelRuleDirection());
+
+            Assert.assertEquals("NeutronMeteringLabelRule JAXB Test 4: Testing excluded failed", false,
+                    testObject.getMeteringLabelRuleExcluded());
+
+            Assert.assertEquals("NeutronMeteringLabelRule JAXB Test 5: Testing id failed",
+                    "4e8e5957-649f-477b-9e5b-f1f75b21c03c", testObject.getMeteringLabelRuleUUID());
+        } catch (Exception e) {
+            Assert.fail("Test failed");
+        }
+    }
+
+}
