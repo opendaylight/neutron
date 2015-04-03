e263b9da2d6e50ffa70787f485402ee0604d150d
bhavesh.kumar1 <bhavesh.kumar1@tcs.com>
2015-04-03 16:08:39 +0530

    JUnit JAXB tests for NeutronSecurity
    Added JUnit JAXB tests for NeutronSecurityGroup and NeutronSecurityRule.
    
    Change-Id: I67b7e7ab0bd9d29a7c7fab8cc4fcf27055def178
    Signed-off-by: bhavesh.kumar1 <bhavesh.kumar1@tcs.com>

---
+++ neutron-spi/src/test/java/org/opendaylight/neutron/spi/NeutronSecurityRuleJAXBTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2015 IBM, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NeutronSecurityRuleJAXBTest {
+	
+	private static final String NeutronSecurityRule_sourceJson="{" +
+	    "\"id\": \"2bc0accf-312e-429a-956e-e4407625eb62\", " +
+	    "\"direction\": \"ingress\", " +
+	    "\"protocol\": \"tcp\", " +
+	    "\"port_range_min\": 80, " +
+	    "\"port_range_max\": 80, " +
+	    "\"ethertype\": \"IPv4\", " +
+	    "\"remote_ip_prefix\": \"0.0.0.0/0\", " +
+	    "\"remote_group_id\": \"85cc3048-abc3-43cc-89b3-377341426ac5\", " +
+	    "\"security_group_id\": \"a7734e61-b545-452d-a3cd-0189cbd9747a\", " +
+	    "\"tenant_id\": \"e4f50856753b4dc6afee5fa6b9b6c550\" }";
+
+	@Test
+	public void test_NeutronSecurityRule_JAXB() {
+		NeutronSecurityRule dummyObject = new NeutronSecurityRule();
+		try {
+			NeutronSecurityRule testObject = (NeutronSecurityRule) JaxbTestHelper.jaxbUnmarshall(dummyObject, NeutronSecurityRule_sourceJson);
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 1: Testing id failed",
+                  "2bc0accf-312e-429a-956e-e4407625eb62", testObject.getSecurityRuleUUID());
+
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 2: Testing direction failed",
+                  "ingress", testObject.getSecurityRuleDirection());
+
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 3: Testing protocol failed",
+                  "tcp", testObject.getSecurityRuleProtocol());
+
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 4: Testing port range min failed",
+            		new Integer(80), testObject.getSecurityRulePortMin());
+
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 5: Testing port range max failed",
+            		new Integer(80), testObject.getSecurityRulePortMax());
+
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 6: Testing ethertype failed",
+                  "IPv4", testObject.getSecurityRuleEthertype());
+
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 7: Testing remote ip prefix failed",
+                   "0.0.0.0/0", testObject.getSecurityRuleRemoteIpPrefix());
+
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 8: Testing remote group id failed",
+                  "85cc3048-abc3-43cc-89b3-377341426ac5", testObject.getSecurityRemoteGroupID());
+            
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 9: Testing security group id failed",
+                    "a7734e61-b545-452d-a3cd-0189cbd9747a", testObject.getSecurityRuleGroupID());
+            
+            Assert.assertEquals("NeutronSecurityRule JAXB Test 10: Testing tenant id failed",
+                    "e4f50856753b4dc6afee5fa6b9b6c550", testObject.getSecurityRuleTenantID());
+		} catch (Exception e) {
+			Assert.fail("Tests failed");
+        }
+	}
+}
