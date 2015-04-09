a601337ace3b83ad924a6a81c495243da893887e
Ryan Moats <rmoats@us.ibm.com>
2015-04-09 11:12:23 -0500

    Add implementation of NeutronVPNIKEPoliciesNorthbound
    Change-Id: I9e89fddcdc534d6abd9bd852ff929de7daec4bc5
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/INeutronVPNIKEPolicyCRUD.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2014 Red Hat, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import java.util.List;
+
+public interface INeutronVPNIKEPolicyCRUD {
+
+    /**
+     * Applications call this interface method to determine if a particular
+     *NeutronVPNIKEPolicy object exists
+     *
+     * @param uuid
+     *            UUID of the NeutronVPNIKEPolicy object
+     * @return boolean
+     */
+
+    public boolean neutronVPNIKEPolicyExists(String uuid);
+
+    /**
+     * Applications call this interface method to return if a particular
+     * NeutronVPNIKEPolicy object exists
+     *
+     * @param uuid
+     *            UUID of the NeutronVPNIKEPolicy object
+     * @return {@link org.opendaylight.neutron.neutron.spi.NeutronVPNIKEPolicy}
+     *          OpenStackNeutronVPNIKEPolicy class
+     */
+
+    public NeutronVPNIKEPolicy getNeutronVPNIKEPolicy(String uuid);
+
+    /**
+     * Applications call this interface method to return all NeutronVPNIKEPolicy objects
+     *
+     * @return List of OpenStackNetworks objects
+     */
+
+    public List<NeutronVPNIKEPolicy> getAllNeutronVPNIKEPolicies();
+
+    /**
+     * Applications call this interface method to add a NeutronVPNIKEPolicy object to the
+     * concurrent map
+     *
+     * @param input
+     *            OpenStackNetwork object
+     * @return boolean on whether the object was added or not
+     */
+
+    public boolean addNeutronVPNIKEPolicy(NeutronVPNIKEPolicy input);
+
+    /**
+     * Applications call this interface method to remove a Neutron NeutronVPNIKEPolicy object to the
+     * concurrent map
+     *
+     * @param uuid
+     *            identifier for the NeutronVPNIKEPolicy object
+     * @return boolean on whether the object was removed or not
+     */
+
+    public boolean removeNeutronVPNIKEPolicy(String uuid);
+
+    /**
+     * Applications call this interface method to edit a NeutronVPNIKEPolicy object
+     *
+     * @param uuid
+     *            identifier of the NeutronVPNIKEPolicy object
+     * @param delta
+     *            OpenStackNeutronVPNIKEPolicy object containing changes to apply
+     * @return boolean on whether the object was updated or not
+     */
+
+    public boolean updateNeutronVPNIKEPolicy(String uuid, NeutronVPNIKEPolicy delta);
+
+    /**
+     * Applications call this interface method to see if a MAC address is in use
+     *
+     * @param uuid
+     *            identifier of the NeutronVPNIKEPolicy object
+     * @return boolean on whether the macAddress is already associated with a
+     * port or not
+     */
+
+    public boolean neutronVPNIKEPolicyInUse(String uuid);
+
+}
