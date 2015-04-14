5ae2da9e8a0714478bdd7c3c973230e9f8916096
Ryan Moats <rmoats@us.ibm.com>
2015-04-14 07:22:31 -0500

    Make dummy provider actually work
    Change-Id: I3921aa4e0c4ffe3ac0fdc5f7bf5a3e7b6634d609
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

--- dummyprovider/src/main/java/org/opendaylight/neutron/dummyprovider/Activator.java
+++ dummyprovider/src/main/java/org/opendaylight/neutron/dummyprovider/Activator.java
@@ -32,26 +32,60 @@ import org.opendaylight.neutron.spi.INeutronSubnetAware;
 
 import org.osgi.framework.BundleContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Activator extends DependencyActivatorBase {
+
+    private static final Logger logger = LoggerFactory.getLogger(Activator.class);
 
     @Override
     public void init(BundleContext context, DependencyManager manager) throws Exception {
        manager.add(createComponent().setInterface(new String[] {
-           INeutronFirewallAware.class.getName(),
-           INeutronFirewallPolicyAware.class.getName(),
-           INeutronFirewallRuleAware.class.getName(),
-           INeutronFloatingIPAware.class.getName(),
-           INeutronLoadBalancerAware.class.getName(),
-           INeutronLoadBalancerHealthMonitorAware.class.getName(),
-           INeutronLoadBalancerListenerAware.class.getName(),
-           INeutronLoadBalancerPoolAware.class.getName(),
-           INeutronLoadBalancerPoolMemberAware.class.getName(),
-           INeutronNetworkAware.class.getName(),
-           INeutronPortAware.class.getName(),
-           INeutronRouterAware.class.getName(),
-           INeutronSecurityGroupAware.class.getName(),
-           INeutronSecurityRuleAware.class.getName(),
-           INeutronSubnetAware.class.getName() }, null));
+           INeutronFirewallAware.class.getName()}, null)
+           .setImplementation(NeutronFirewallDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronFirewallPolicyAware.class.getName()}, null)
+           .setImplementation(NeutronFirewallPolicyDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronFirewallRuleAware.class.getName()}, null)
+           .setImplementation(NeutronFirewallRuleDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronFloatingIPAware.class.getName()}, null)
+           .setImplementation(NeutronFloatingIPDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronLoadBalancerAware.class.getName()}, null)
+           .setImplementation(NeutronLoadBalancerDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronLoadBalancerHealthMonitorAware.class.getName()}, null)
+           .setImplementation(NeutronLoadBalancerHealthMonitorDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronLoadBalancerListenerAware.class.getName()}, null)
+           .setImplementation(NeutronLoadBalancerListenerDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronLoadBalancerPoolAware.class.getName()}, null)
+           .setImplementation(NeutronLoadBalancerPoolDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronLoadBalancerPoolMemberAware.class.getName()}, null)
+           .setImplementation(NeutronLoadBalancerPoolMemberDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronNetworkAware.class.getName()}, null)
+           .setImplementation(NeutronNetworkDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronPortAware.class.getName()}, null)
+           .setImplementation(NeutronPortDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronRouterAware.class.getName()}, null)
+           .setImplementation(NeutronRouterDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronSecurityGroupAware.class.getName()}, null)
+           .setImplementation(NeutronSecurityGroupDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronSecurityRuleAware.class.getName()}, null)
+           .setImplementation(NeutronSecurityRuleDummyProvider.class));
+       manager.add(createComponent().setInterface(new String[] {
+           INeutronSubnetAware.class.getName()}, null)
+           .setImplementation(NeutronSubnetDummyProvider.class));
     }
 
     /**
--- dummyprovider/src/main/java/org/opendaylight/neutron/dummyprovider/NeutronFirewallPolicyDummyProvider.java
+++ dummyprovider/src/main/java/org/opendaylight/neutron/dummyprovider/NeutronFirewallPolicyDummyProvider.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (C) 2015 IBM, Inc.
+ * Copyright IBM Corporation, 2015.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.neutron.spi;
+package org.opendaylight.neutron.dummyprovider;
 
 import org.opendaylight.neutron.spi.INeutronFirewallPolicyAware;
 import org.opendaylight.neutron.spi.NeutronFirewallPolicy;
