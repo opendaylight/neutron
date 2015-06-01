cd6b718f3de97737eea21b92129529706dfb38d1
Ryan Moats <rmoats@us.ibm.com>
2015-06-01 15:55:06 -0500

    Yet more classes for E2E testing
    This time adding LBaaS and VPNaaS classes...
    
    Patch Set 5 works, but without metering and VPNaaS...
    
    Patch Set 8 adds tests of metering lables and rules correctly
    
    Patch Set 17 adds testing of VPNaaS items (- IPSECPolicy)
    
    Subsequent patches are aimed at resolving the issues
    with NB registration of IPSECPolicy and LBaaS URL issues
    
    Change-Id: I41a08cc974c71c5f9bf207c8ea7e4e3600fee494
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

---
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronLBPoolMembersTests.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 IBM, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.e2etest;
+
+import java.io.OutputStreamWriter;
+
+import java.lang.Thread;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.Assert;
+
+public class NeutronLBPoolMembersTests {
+    String base;
+
+    public NeutronLBPoolMembersTests(String base) {
+        this.base = base;
+    }
+
+// TODO: have to be careful here as the pool id is included
+
+/*    public void pool_collection_get_test() {
+        String url_s = base + "/pools";
+        try {
+            URL url = new URL(url_s);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setRequestMethod("GET");
+            httpConn.setRequestProperty("Content-Type", "application/json");
+            httpConn.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=");
+            Assert.assertEquals("LB Pool Collection GET failed",
+                        200, httpConn.getResponseCode());
+        } catch (Exception e) {
+            Assert.assertFalse("E2E Tests Failed", true);
+        }
+    } */
+}
