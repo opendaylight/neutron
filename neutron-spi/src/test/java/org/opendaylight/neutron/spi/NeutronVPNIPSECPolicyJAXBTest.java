152b6f1680a790e59088f87c7fd7304e992b171e
Ryan Moats <rmoats@us.ibm.com>
2015-06-01 15:55:06 -0500

    Yet more classes for E2E testing
    This time adding LBaaS and VPNaaS classes...
    
    Patch Set 5 works, but without metering and VPNaaS...
    
    Patch Set 8 adds tests of metering lables and rules correctly
    
    Subsequent patches are aimed at resolving the issues
    with NB registration of VPNaaS (and possibly
    fixing the LBaaS rooting issues)
    
    Change-Id: I41a08cc974c71c5f9bf207c8ea7e4e3600fee494
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

--- neutron-spi/src/test/java/org/opendaylight/neutron/spi/NeutronVPNIPSECPolicyJAXBTest.java
+++ neutron-spi/src/test/java/org/opendaylight/neutron/spi/NeutronVPNIPSECPolicyJAXBTest.java
@@ -15,54 +15,54 @@ import org.opendaylight.neutron.spi.NeutronVPNIPSECPolicy;
 
 public class NeutronVPNIPSECPolicyJAXBTest {
 
-    private static final String NeutronVPNISECPolicy_SingleProvider_sourceJson = "{" +
+    private static final String NeutronVPNIPSECPolicy_SingleProvider_sourceJson = "{" +
         "\"id\": \"5291b189-fd84-46e5-84bd-78f40c05d69c\", " +
         "\"tenant_id\": \"ccb81365fe36411a9011e90491fe1330\", " +
         "\"name\": \"ipsecpolicy1\", " +
         "\"description\": \"update description\", " +
         "\"transform_protocol\": \"esp\", " +
-        "\"encapsulaion_mode\": \"tunnel\", " +
+        "\"encapsulation_mode\": \"tunnel\", " +
         "\"auth_algorithm\": \"sha1\", " +
         "\"encryption_algorithm\": \"aes-128\", " +
         "\"pfs\": \"group5\", " +
         "\"lifetime\": { " + "\"units\": \"seconds\", " + "\"value\": 3600 " + "} }";
 
     @Test
-    public void test_NeutronVPNISECPolicy_JAXB() {
+    public void test_NeutronVPNIPSECPolicy_JAXB() {
         NeutronVPNIPSECPolicy dummyObject = new NeutronVPNIPSECPolicy();
         try {
-            NeutronVPNIPSECPolicy testObject = (NeutronVPNIPSECPolicy) JaxbTestHelper.jaxbUnmarshall(dummyObject, NeutronVPNISECPolicy_SingleProvider_sourceJson);
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 1: Testing id failed",
+            NeutronVPNIPSECPolicy testObject = (NeutronVPNIPSECPolicy) JaxbTestHelper.jaxbUnmarshall(dummyObject, NeutronVPNIPSECPolicy_SingleProvider_sourceJson);
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 1: Testing id failed",
                   "5291b189-fd84-46e5-84bd-78f40c05d69c", testObject.getID());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 2: Testing tenant id failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 2: Testing tenant id failed",
                   "ccb81365fe36411a9011e90491fe1330", testObject.getTenantID());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 3: Testing name failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 3: Testing name failed",
                   "ipsecpolicy1", testObject.getName());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 4: Testing description failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 4: Testing description failed",
                   "update description", testObject.getDescription());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 5: Testing transform protocol failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 5: Testing transform protocol failed",
                   "esp", testObject.getTransformProtocol());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 6: Testing encapsulation mode failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 6: Testing encapsulation mode failed",
                   "tunnel", testObject.getEncapsulationMode());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 7: Testing authorization algorithm failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 7: Testing authorization algorithm failed",
                   "sha1", testObject.getAuthAlgorithm());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 8: Testing encryption algorithm failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 8: Testing encryption algorithm failed",
                   "aes-128", testObject.getEncryptionAlgorithm());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 9: Testing PerfectForwardSecrecy failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 9: Testing PerfectForwardSecrecy failed",
                   "group5", testObject.getPerfectForwardSecrecy());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 10: Testing Lifetime unit value failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 10: Testing Lifetime unit value failed",
                   "seconds", testObject.getLifetime().getUnits());
 
-            Assert.assertEquals("NeutronVPNISECPolicy JAXB Test 11: Testing Lifetime value failed",
+            Assert.assertEquals("NeutronVPNIPSECPolicy JAXB Test 11: Testing Lifetime value failed",
                   new Integer(3600), testObject.getLifetime().getValue());
         } catch (Exception e) {
             Assert.fail("Tests failed");
