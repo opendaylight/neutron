3586a6f5de3c8e1ab3b75363dbd29fc80e033ae4
Ryan Moats <rmoats@us.ibm.com>
2015-06-03 10:11:42 -0500

    Add Port create test in response to Bug 3368
    Change-Id: Ib9009aefcbe3a6548a61fd1bb2fe710ebba3bb53
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

--- integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronSubnetTests.java
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronSubnetTests.java
@@ -38,4 +38,40 @@ public class NeutronSubnetTests {
             Assert.assertFalse("E2E Tests Failed", true);
         }
     }
+
+    //TODO handle SB check
+    public void singleton_subnet_create_test() {
+        String url_s = base + "/subnets";
+        String content = " { \"subnet\": { "+
+            "\"name\": \"\", "+
+            "\"enable_dhcp\": true, "+
+            "\"network_id\": \"4e8e5957-649f-477b-9e5b-f1f75b21c03c\", "+
+            "\"tenant_id\": \"9bacb3c5d39d41a79512987f338cf177\", "+
+            "\"dns_nameservers\": [], "+
+            "\"allocation_pools\": [ { "+
+                "\"start\": \"10.0.0.2\", "+
+                "\"end\": \"10.0.0.254\" } ], "+
+            "\"host_routes\": [], "+
+            "\"ip_version\": 4, "+
+            "\"gateway_ip\": \"10.0.0.1\", "+
+            "\"cidr\": \"10.0.0.0/24\", "+
+            "\"id\": \"3b80198d-4f7b-4f77-9ef5-774d54e17126\" } } ";
+
+        try {
+            URL url = new URL(url_s);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setRequestMethod("POST");
+            httpConn.setRequestProperty("Content-Type", "application/json");
+            httpConn.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=");
+            httpConn.setDoOutput(true);
+            OutputStreamWriter out = new OutputStreamWriter(
+                httpConn.getOutputStream());
+            out.write(content);
+            out.close();
+            Assert.assertEquals("Singleton Network Post Failed NB",
+                201, httpConn.getResponseCode());
+        } catch (Exception e) {
+            Assert.assertFalse("E2E Tests Failed", true);
+        }
+    }
 }
