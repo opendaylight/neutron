7c0892146ba59a3505762575f95fb38eac488059
Ryan Moats <rmoats@us.ibm.com>
2015-06-08 11:39:54 -0500

    Add Metering Create Tests for Bug 2546
    Change-Id: If038abf9b6e1bd37f1cace1111e0666bb7f6b0e6
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

--- integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronMeteringRuleTests.java
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronMeteringRuleTests.java
@@ -38,4 +38,32 @@ public class NeutronMeteringRuleTests {
             Assert.assertFalse("E2E Tests Failed", true);
         }
     }
+
+    //TODO handle SB check
+    public void singleton_meteringLabel_create_test() {
+        String url_s = base + "/metering/metering-label-rules";
+        String content = "{ \"metering_label_rule\": { " +
+            "\"remote_ip_prefix\": \"10.0.1.0/24\", " +
+            "\"direction\": \"ingress\", " +
+            "\"metering_label_id\": \"bc91b832-8465-40a7-a5d8-ba87de442266\", " +
+            "\"id\": \"00e13b58-b4f2-4579-9c9c-7ac94615f9ae\", " +
+            "\"excluded\": false } }";
+
+        try {
+            URL url = new URL(url_s);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setRequestMethod("POST");
+            httpConn.setRequestProperty("Content-Type", "application/json");
+            httpConn.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=");
+            httpConn.setDoOutput(true);
+            OutputStreamWriter out = new OutputStreamWriter(
+                httpConn.getOutputStream());
+            out.write(content);
+            out.close();
+            Assert.assertEquals("Singleton Metering Rule Post Failed NB",
+                201, httpConn.getResponseCode());
+        } catch (Exception e) {
+            Assert.assertFalse("E2E Tests Failed", true);
+        }
+    }
 }
