bcf6604d358784b8c894aab4c54231eab8db380d
Ryan Moats <rmoats@us.ibm.com>
2015-06-08 09:01:46 -0500

    Test code for Bug #2545
    Added extra tests for stable/lithium
    
    Change-Id: Ifdec5ed2d83616bb5d4a72d4378a77cd0c9bc4b3
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

--- integration/test/src/test/java/org/opendaylight/neutron/e2etest/Li_NeutronLBListenerTests.java
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/Li_NeutronLBListenerTests.java
@@ -38,4 +38,38 @@ public class Li_NeutronLBListenerTests {
             Assert.assertFalse("E2E Tests Failed", true);
         }
     }
+
+    //TODO handle SB check
+    public void singleton_lb_listener_create_test() {
+        String url_s = base + "/listeners";
+        String content = "{ \"listener\": { " +
+            "\"admin_state_up\": true, " +
+            "\"connection_limit\": 100, " +
+            "\"default_pool_id\": null, " +
+            "\"description\": \"listener one\", " +
+            "\"id\": \"39de4d56-d663-46e5-85a1-5b9d5fa1782a\", " +
+            "\"loadbalancers\": [ { " +
+                "\"id\": \"a36c20d0-18e9-42ce-88fd-82a35977ee8d\" } ], " +
+            "\"name\": \"listener1\", " +
+            "\"protocol\": \"HTTP\", " +
+            "\"protocol_port\": 80, " +
+            "\"tenant_id\": \"b7c1a69e88bf4b21a8148f787aef2081\" } }";
+
+        try {
+            URL url = new URL(url_s);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setRequestMethod("POST");
+            httpConn.setRequestProperty("Content-Type", "application/json");
+            httpConn.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=");
+            httpConn.setDoOutput(true);
+            OutputStreamWriter out = new OutputStreamWriter(
+                httpConn.getOutputStream());
+            out.write(content);
+            out.close();
+            Assert.assertEquals("Old URI Singleton LB Listener Post Failed NB",
+                201, httpConn.getResponseCode());
+        } catch (Exception e) {
+            Assert.assertFalse("E2E Tests Failed", true);
+        }
+    }
 }
