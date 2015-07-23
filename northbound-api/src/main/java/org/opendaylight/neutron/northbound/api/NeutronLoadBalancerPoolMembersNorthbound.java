386675d049ed72e38b02824fe93e4fac7c67acb2
Ryan Moats <rmoats@us.ibm.com>
2015-07-22 22:23:25 -0500

    Fix create of LBaaS pool members
    LBaaS pool members should be created by POST,
    not PUT.
    
    Change-Id: I8ba68031f6869c7f2fd1d5528e713b623eca0fd1
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

--- northbound-api/src/main/java/org/opendaylight/neutron/northbound/api/NeutronLoadBalancerPoolMembersNorthbound.java
+++ northbound-api/src/main/java/org/opendaylight/neutron/northbound/api/NeutronLoadBalancerPoolMembersNorthbound.java
@@ -20,6 +20,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -170,7 +171,7 @@ public Response showLoadBalancerPoolMember(
 /**
  * Adds a Member to an LBaaS Pool member
  */
-@PUT
+@POST
 @Produces({MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_JSON})
 @StatusCodes({
