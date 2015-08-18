5cd1fdc3deffde7b947ea60ced7ff0ec3de083a0
Ryan Moats <rmoats@us.ibm.com>
2015-08-18 09:54:52 -0500

    Add IT cases for Metering Labels
    Add missing CRUD operations to IT for Metering Labels
    
    Change-Id: Iafeef55b46e0c46723c363f59784ba403dfa77ac
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

--- integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronMeteringLabelTests.java
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronMeteringLabelTests.java
@@ -40,8 +40,27 @@ public class NeutronMeteringLabelTests {
         ITNeutronE2E.test_create(url, content, "Singleton Metering Label Post Failed NB");
     }
 
+    public void meteringLabel_element_get_test() {
+        String url = base + "/metering/metering-labels/bc91b832-8465-40a7-a5d8-ba87de442266";
+        ITNeutronE2E.test_fetch(url, true ,"Metering Label Element Get Failed");
+    }
+
+    public void meteringLabel_delete_test() {
+        String url = base + "/metering/metering-labels/bc91b832-8465-40a7-a5d8-ba87de442266";
+        ITNeutronE2E.test_delete(url, "Metering Label Element Delete Failed");
+    }
+
+    public void meteringLabel_element_negative_get_test() {
+        String url = base + "/metering/metering-labels/bc91b832-8465-40a7-a5d8-ba87de442266";
+        ITNeutronE2E.test_fetch(url, false ,"Metering Label Element Negative Get Failed");
+    }
+
     public static void runTests(String base) {
         NeutronMeteringLabelTests meteringLabel_tester = new NeutronMeteringLabelTests(base);
         meteringLabel_tester.meteringLabel_collection_get_test();
+        meteringLabel_tester.singleton_meteringLabel_create_test();
+        meteringLabel_tester.meteringLabel_element_get_test();
+        meteringLabel_tester.meteringLabel_delete_test();
+        meteringLabel_tester.meteringLabel_element_negative_get_test();
     }
 }
