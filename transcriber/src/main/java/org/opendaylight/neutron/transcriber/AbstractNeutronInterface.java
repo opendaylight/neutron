3a3fc437ea23a1b1593d2b038caf8455e30dd120
Isaku Yamahata <isaku.yamahata@intel.com>
2015-09-10 16:37:08 -0700

    transcriber: use transaction chain for optimization
    This patch makes transcriber use transaction chain.
    The next step is to make subclasses use transaction
    chain.
    
    Related-Bug: 4143
    Change-Id: I2f79877259acf8f642ffa4877552112f7b0da7b5
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- transcriber/src/main/java/org/opendaylight/neutron/transcriber/AbstractNeutronInterface.java
+++ transcriber/src/main/java/org/opendaylight/neutron/transcriber/AbstractNeutronInterface.java
@@ -12,11 +12,15 @@ import java.lang.reflect.Method;
 
 import java.util.concurrent.ExecutionException;
 
+import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChain;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
@@ -31,7 +35,7 @@ import com.google.common.util.concurrent.CheckedFuture;
 
 import org.opendaylight.neutron.spi.INeutronObject;
 
-public abstract class AbstractNeutronInterface<T extends DataObject, S extends INeutronObject> implements AutoCloseable {
+public abstract class AbstractNeutronInterface<T extends DataObject, S extends INeutronObject> implements AutoCloseable, TransactionChainListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractNeutronInterface.class);
     private static final int DEDASHED_UUID_LENGTH = 32;
     private static final int DEDASHED_UUID_START = 0;
@@ -50,15 +54,92 @@ public abstract class AbstractNeutronInterface<T extends DataObject, S extends I
         return db;
     }
 
+    public void onTransactionChainFailed(TransactionChain<?, ?> chain, AsyncTransaction<?, ?> transaction, Throwable cause) {
+        LOGGER.error("Broken chain {} in TxchainDomWrite, transaction {}, cause {}",
+                     chain, transaction.getIdentifier(), cause);
+    }
+
+    public void onTransactionChainSuccessful(TransactionChain<?, ?> chain) {
+        LOGGER.debug("Chain {} closed successfully", chain);
+    }
+
+    protected BindingTransactionChain createTransactionChain() {
+        return getDataBroker().createTransactionChain(this);
+    }
+
+    protected interface Action0<U> {
+        public U action(BindingTransactionChain chain);
+    }
+
+    protected <U> U chainWrapper0(BindingTransactionChain chain,
+                                  Action0<U> actor) {
+        if (chain != null) {
+            return actor.action(chain);
+        }
+        try (BindingTransactionChain newChain = this.createTransactionChain()) {
+            return actor.action(newChain);
+        }
+    }
+
+    protected interface Action1<U, V> {
+        public U action(V input, BindingTransactionChain chain);
+    }
+
+    protected <U, V> U chainWrapper1(V input, BindingTransactionChain chain,
+                                     Action1<U, V> actor) {
+        if (chain != null) {
+            return actor.action(input, chain);
+        }
+        try (BindingTransactionChain newChain = this.createTransactionChain()) {
+            return actor.action(input, newChain);
+        }
+    }
+
+    protected interface Action2<U, V, W> {
+        public U action(V input0, W input1, BindingTransactionChain chain);
+    }
+
+    protected <U, V, W> U chainWrapper2(V input0, W input1,
+                                        BindingTransactionChain chain,
+                                        Action2<U, V, W> actor) {
+        if (chain != null) {
+            return actor.action(input0, input1, chain);
+        }
+        try (BindingTransactionChain newChain = this.createTransactionChain()) {
+            return actor.action(input0, input1, newChain);
+        }
+    }
+
+    private boolean _exists(String uuid, BindingTransactionChain chain) {
+        Preconditions.checkNotNull(chain);
+        T dataObject = readMd(createInstanceIdentifier(toMd(uuid)), chain);
+        return dataObject != null;
+    }
+
+    public boolean exists(String uuid, BindingTransactionChain chain) {
+        return chainWrapper1(uuid, chain,
+                             new Action1<Boolean, String>() {
+                                 @Override
+                                 public Boolean action(String uuid, BindingTransactionChain chain) {
+                                     return _exists(uuid, chain);
+                                 }
+                             }).booleanValue();
+    }
+
+    public boolean exists(String uuid) {
+        return exists(uuid, null);
+    }
+
     protected abstract InstanceIdentifier<T> createInstanceIdentifier(T item);
 
     protected abstract T toMd(S neutronObject);
 
     protected abstract T toMd(String uuid);
 
-    protected <T extends org.opendaylight.yangtools.yang.binding.DataObject> T readMd(InstanceIdentifier<T> path) {
+    private <T extends org.opendaylight.yangtools.yang.binding.DataObject> T _readMd(InstanceIdentifier<T> path, BindingTransactionChain chain) {
+        Preconditions.checkNotNull(chain);
         T result = null;
-        final ReadOnlyTransaction transaction = getDataBroker().newReadOnlyTransaction();
+        final ReadOnlyTransaction transaction = chain.newReadOnlyTransaction();
         CheckedFuture<Optional<T>, ReadFailedException> future = transaction.read(LogicalDatastoreType.CONFIGURATION, path);
         if (future != null) {
             Optional<T> optional;
@@ -75,13 +156,32 @@ public abstract class AbstractNeutronInterface<T extends DataObject, S extends I
         return result;
     }
 
-    protected boolean addMd(S neutronObject) {
-        // TODO think about adding existence logic
-        return updateMd(neutronObject);
+    protected <T extends org.opendaylight.yangtools.yang.binding.DataObject> T readMd(InstanceIdentifier<T> path, BindingTransactionChain chain) {
+        return chainWrapper1(path, chain,
+                             new Action1<T, InstanceIdentifier<T>>() {
+                                 @Override
+                                 public T action(InstanceIdentifier<T> path, BindingTransactionChain chain) {
+                                     return _readMd(path, chain);
+                                 }
+                             });
     }
 
-    protected boolean updateMd(S neutronObject) {
-        WriteTransaction transaction = getDataBroker().newWriteOnlyTransaction();
+    protected <T extends org.opendaylight.yangtools.yang.binding.DataObject> T readMd(InstanceIdentifier<T> path) {
+        return readMd(path, null);
+    }
+
+    protected boolean addMd(S neutronObject, BindingTransactionChain chain) {
+        // TODO think about adding existence logic
+        return updateMd(neutronObject, chain);
+    }
+
+    protected boolean addMd(S neutronObject) {
+        return addMd(neutronObject, null);
+    }
+
+   protected boolean _updateMd(S neutronObject, BindingTransactionChain chain) {
+        Preconditions.checkNotNull(chain);
+        WriteTransaction transaction = chain.newWriteOnlyTransaction();
         T item = toMd(neutronObject);
         InstanceIdentifier<T> iid = createInstanceIdentifier(item);
         transaction.put(LogicalDatastoreType.CONFIGURATION, iid, item,true);
@@ -95,8 +195,23 @@ public abstract class AbstractNeutronInterface<T extends DataObject, S extends I
         return true;
     }
 
-    protected boolean removeMd(T item) {
-        WriteTransaction transaction = getDataBroker().newWriteOnlyTransaction();
+    protected boolean updateMd(S neutronObject, BindingTransactionChain chain) {
+        return chainWrapper1(neutronObject, chain,
+                             new Action1<Boolean, S>() {
+                                 @Override
+                                 public Boolean action(S neutronObject, BindingTransactionChain chain) {
+                                     return _updateMd(neutronObject, chain);
+                                 }
+                             }).booleanValue();
+    }
+
+    protected boolean updateMd(S neutronObject) {
+        return updateMd(neutronObject, null);
+    }
+
+    private boolean _removeMd(T item, BindingTransactionChain chain) {
+        Preconditions.checkNotNull(chain);
+        WriteTransaction transaction = chain.newWriteOnlyTransaction();
         InstanceIdentifier<T> iid = createInstanceIdentifier(item);
         transaction.delete(LogicalDatastoreType.CONFIGURATION, iid);
         CheckedFuture<Void, TransactionCommitFailedException> future = transaction.submit();
@@ -107,6 +222,20 @@ public abstract class AbstractNeutronInterface<T extends DataObject, S extends I
             return false;
         }
         return true;
+    }
+
+    protected boolean removeMd(T item, BindingTransactionChain chain) {
+        return chainWrapper1(item, chain,
+                             new Action1<Boolean, T>() {
+                                 @Override
+                                 public Boolean action(T item, BindingTransactionChain chain) {
+                                     return _removeMd(item, chain);
+                                 }
+                             }).booleanValue();
+    }
+
+    protected boolean removeMd(T item) {
+        return removeMd(item, null);
     }
 
     protected Uuid toUuid(String uuid) {
