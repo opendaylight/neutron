fce1d051ad4bc5fa733321c6953d5d4cd8355971
Isaku Yamahata <isaku.yamahata@intel.com>
2015-10-09 15:46:33 -0700

    use INeutronObject instead of NeutronObject where possible
    Change-Id: Id5abdaa58af68985d4ec88ecfd4a1e0c9a60b653
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- northbound-api/src/main/java/org/opendaylight/neutron/northbound/api/AbstractNeutronNorthboundIAware.java
+++ northbound-api/src/main/java/org/opendaylight/neutron/northbound/api/AbstractNeutronNorthboundIAware.java
@@ -14,7 +14,7 @@ import java.util.List;
 import org.codehaus.enunciate.jaxrs.ResponseCode;
 import org.codehaus.enunciate.jaxrs.StatusCodes;
 import org.opendaylight.neutron.spi.INeutronCRUD;
-import org.opendaylight.neutron.spi.NeutronObject;
+import org.opendaylight.neutron.spi.INeutronObject;
 
 import javax.ws.rs.core.Response;
 
@@ -23,7 +23,7 @@ import org.slf4j.LoggerFactory;
 
 // When INeutron*Aware interfaces are deleted, this class should be deleted for BORON
 @Deprecated
-public abstract class AbstractNeutronNorthboundIAware<T extends NeutronObject, NeutronRequest extends INeutronRequest<T>, I extends INeutronCRUD<T>, INeutronAware>
+public abstract class AbstractNeutronNorthboundIAware<T extends INeutronObject, NeutronRequest extends INeutronRequest<T>, I extends INeutronCRUD<T>, INeutronAware>
     extends AbstractNeutronNorthbound<T, NeutronRequest, I> {
     private static final Logger LOGGER = LoggerFactory
         .getLogger(AbstractNeutronNorthboundIAware.class);
