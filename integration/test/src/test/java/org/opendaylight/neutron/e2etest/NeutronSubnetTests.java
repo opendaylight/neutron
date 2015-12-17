201cbd68375281119a8c13e85c5dbe44f58a1844
Vishal Thapar <vishal.thapar@ericsson.com>
2015-12-18 02:20:23 +0530

    Bug 4805 add host_routes info to MDSAL
    Transcriber is missing code to add hostRoute information to
    MDSAL when Subnet is created/updated.
    
    This change adds code to toMd() and fromMd() to handle hostRoutes.
    
    Change-Id: I7b7b7dc5092971d86aec38e5365518712d84e8c2
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronSubnetTests.java
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronSubnetTests.java
@@ -37,12 +37,12 @@ public class NeutronSubnetTests {
             "\"enable_dhcp\": true, "+
             "\"network_id\": \"4e8e5957-649f-477b-9e5b-f1f75b21c03c\", "+
             "\"tenant_id\": \"9bacb3c5d39d41a79512987f338cf177\", "+
-            "\"dns_nameservers\": [], "+
+            "\"dns_nameservers\": [\"8.8.8.8"], "+
             "\"allocation_pools\": [ { "+
                 "\"start\": \"10.0.0.2\", "+
                 "\"end\": \"10.0.0.254\" } ], "+
             "\"host_routes\":[ { \"destination\":\"0.0.0.0/0\", " +
-               " \"nexthop\":\"123.456.78.9\" }, " +
+               " \"nexthop\":\"123.156.78.9\" }, " +
             " { \"destination\":\"192.168.0.0/24\", " +
                " \"nexthop\":\"192.168.0.1\" } ], " +
             "\"ip_version\": 4, "+
