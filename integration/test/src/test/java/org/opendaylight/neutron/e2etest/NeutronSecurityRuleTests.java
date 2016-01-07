e1b4d7fa2b4be6f39aa7ca3f2bf394abb49d977f
Ritu Sood <ritu.sood@intel.com>
2016-01-06 19:43:23 -0800

    Add more tests to improve code coverage
    Adding new tests 1)update and 2)get with query achieved more
    functional coverage. Moving the collection_get_test later after the
    nodes were created helped getting more condition coverage in
    listGroups and listRules functions. Improved code coverage from
    66% to 80% for NeutronSecurityGroupsNorthbound and
    42% to 70% NeutronSecurityRulesNorthbound.
    
    Change-Id: I56708a0c1c66158dd29784a9a2edd99e5749077e
    Signed-off-by: Ritu Sood <ritu.sood@intel.com>

--- integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronSecurityRuleTests.java
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronSecurityRuleTests.java
@@ -80,9 +80,32 @@ public class NeutronSecurityRuleTests {
         ITNeutronE2E.test_create(url, content, "Security Rule Multiple Post Failed");
     }
 
+    public void singleton_sr_modify_test() {
+        String url = base + "/security-group-rules/9b4be7fa-e56e-40fb-9516-1f0fa9185669";
+        String content = " {\"security_group_rule\": " +
+            "{\"remote_group_id\": null, \"direction\": \"egress\", " +
+            "\"remote_ip_prefix\": null, \"protocol\": \"tcp\", " +
+            "\"ethertype\": \"IPv6\", \"tenant_id\": " +
+            "\"00f340c7c3b34ab7be1fc690c05a0275\", \"port_range_max\": 77, " +
+            "\"port_range_min\": 77, " +
+            "\"id\": \"9b4be7fa-e56e-40fb-9516-1f0fa9185669\", " +
+            "\"security_group_id\": " +
+            "\"b60490fe-60a5-40be-af63-1d641381b784\"}}";
+        ITNeutronE2E.test_modify(url, content, "Security Rule Singleton Put Failed");
+    }
+
     public void sr_element_get_test() {
         String url = base + "/security-group-rules/9b4be7fa-e56e-40fb-9516-1f0fa9185669";
         ITNeutronE2E.test_fetch(url, true, "Security Rule Element Get Failed");
+    }
+
+    public void sr_element_get_with_query_test() {
+        String url = base + "/security-group-rules/9b4be7fa-e56e-40fb-9516-1f0fa9185669" +
+            "?fields=id&fields=direction&fields=protocol&fields=port_range_min" +
+            "&fields=port_range_max&fields=ethertype&fields=remote_ip_prefix" +
+            "&fields=remote_group_id&fields=security_group_id&fields=tenant_id" +
+            "&fields=limit&fields=marker&fields=page_reverse";
+        ITNeutronE2E.test_fetch(url, true, "Security Rule Element Get With Query Failed");
     }
 
     public void sr_delete_test() {
@@ -131,10 +154,12 @@ public class NeutronSecurityRuleTests {
 
     public static void runTests(String base) {
         NeutronSecurityRuleTests securityRule_tester = new NeutronSecurityRuleTests(base);
-        securityRule_tester.securityRule_collection_get_test();
         securityRule_tester.singleton_sr_create_test();
         securityRule_tester.multiple_sr_create_test();
+        securityRule_tester.singleton_sr_modify_test();
         securityRule_tester.sr_element_get_test();
+        securityRule_tester.sr_element_get_with_query_test();
+        securityRule_tester.securityRule_collection_get_test();
         securityRule_tester.sr_delete_test();
         securityRule_tester.sr_element_negative_get_test();
         securityRule_tester.bug4043_ipv4_test();
