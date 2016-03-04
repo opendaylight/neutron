f37410e57a43debc3eb38bffb4d405368ea9af00
Isaku Yamahata <isaku.yamahata@intel.com>
2016-03-03 18:03:55 -0800

    WIP: yang model revise: neutron-secgroups.yang
    This patch is to show what will happen early.
    TODO: fix up related java class definition and compile
    breakage.
    
    Change-Id: I7a3c79eae446858d6f0f4b8cd8757c85c1dab370
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Ritu Sood <ritu.sood@intel.com>

--- model/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/neutron/constants/rev150712/NeutronUtils.java
+++ model/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/neutron/constants/rev150712/NeutronUtils.java
@@ -14,6 +14,13 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.FwProtocolIcmp;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.FwProtocolTcp;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.FwProtocolUdp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.ProtocolBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.ProtocolIcmp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.ProtocolIcmpV6;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.ProtocolHttp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.ProtocolHttps;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.ProtocolTcp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev150712.ProtocolUdp;
 
 
 public final class NeutronUtils {
@@ -40,6 +47,30 @@ public final class NeutronUtils {
 
         public static String getName(Class<? extends FwProtocolBase> key) {
             ImmutableBiMap<Class<? extends FwProtocolBase>, String> inverseMapper = MAPPER.inverse();
+            return inverseMapper.get(key);
+        }
+    }
+
+    // For security group
+    public static class ProtocolMapper {
+        private ProtocolMapper() {
+            throw new UnsupportedOperationException("Utility class should not be instantiated.");
+        }
+
+        private static final ImmutableBiMap<String, Class<? extends ProtocolBase>> MAPPER
+            = new ImmutableBiMap.Builder<String, Class<? extends ProtocolBase>>()
+            .put("icmp", ProtocolIcmp.class)
+            .put("tcp", ProtocolTcp.class)
+            .put("udp", ProtocolUdp.class)
+            .put("icmpv6", ProtocolIcmpV6.class)
+            .build();
+
+        public static Class<? extends ProtocolBase> get(String key) {
+            return MAPPER.get(key);
+        }
+
+        public static String getName(Class<? extends ProtocolBase> key) {
+            ImmutableBiMap<Class<? extends ProtocolBase>, String> inverseMapper = MAPPER.inverse();
             return inverseMapper.get(key);
         }
     }
