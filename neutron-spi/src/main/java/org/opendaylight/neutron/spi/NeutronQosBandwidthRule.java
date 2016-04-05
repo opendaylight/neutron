9348cc61819233ceb0f9a84a67a3aa349019f811
pramod <pramod.raghavendra.jayathirth@intel.com>
2016-04-05 16:46:55 -0700

    Adds the "Quality of Service" to the OpenDayLight Neutron
    This patch enables the Quality of Service feature in the OpenDayLight
    Neutron.
    The implementation is done as follows
    1. Defining the YANG model
    2. Implementing of API
    3. Testing the developed API
    
    The QOS is achieved using either limiting the bandwidth or through
    DSCP marking
    
    Data model -
    https://specs.openstack.org/openstack/neutron-specs/specs/liberty/qos-api-extension.html
    This link has the data model changes as suggested by the OpenStack
    Neutron for the QOS feature
    API -
    http://developer.openstack.org/api-ref-networking-v2-ext.html#qos-ext
    This link has the API for the Openstack for the QOS feature
    
    Change-Id: I7074dbb6d494b1267b66a925742e9812ef8a9304
    Signed-off-by: pramod <pramod.raghavendra.jayathirth@intel.com>

---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/NeutronQosBandwidthRule.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.neutron.spi;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public class NeutronQosBandwidthRule extends NeutronObject implements Serializable, INeutronObject {
+    private static final long serialVersionUID = 1L;
+
+    @XmlElement(name = "max_kbps")
+    BigInteger maxKbps;
+
+    @XmlElement(name = "max_burst_kbps")
+    BigInteger maxBurstKbps;
+
+    public BigInteger getMaxKbps() {
+        return maxKbps;
+    }
+
+    public void setMaxKbps(BigInteger maxKbpsValue) {
+        this.maxKbps = maxKbpsValue;
+    }
+
+    public BigInteger getMaxBurstKbps() {
+        return maxBurstKbps;
+    }
+
+    public void setMaxBurstKbps(BigInteger maxBurstKbps) {
+        this.maxBurstKbps = maxBurstKbps;
+    }
+
+    public NeutronQosBandwidthRule extractFields(List<String> fields) {
+        NeutronQosBandwidthRule ans = new NeutronQosBandwidthRule();
+        Iterator<String> i = fields.iterator();
+        while (i.hasNext()) {
+            String s = i.next();
+            if (s.equals("id")) {
+                ans.setID(this.getID());
+            }
+            if (s.equals("tenant_id")) {
+                ans.setTenantID(this.getTenantID());
+            }
+            if (s.equals("max_kbps")) {
+                ans.setMaxKbps(this.getMaxKbps());
+            }
+            if (s.equals("max_burst_kbps")) {
+                ans.setMaxBurstKbps(this.getMaxBurstKbps());
+            }
+        }
+        return ans;
+    }
+
+    @Override
+    public String toString() {
+        return "qosBandwidthRules{" +
+            "qosBandwidthRuleUUID'" + uuid + '\'' +
+            ", qosBandwidthRuleTenantID='" + tenantID + '\'' +
+            ", qosBandwidthMaxValue='" + maxKbps + '\'' +
+            ", qosBandwidthMaxBurst='" + maxBurstKbps +
+            '}';
+    }
+}
