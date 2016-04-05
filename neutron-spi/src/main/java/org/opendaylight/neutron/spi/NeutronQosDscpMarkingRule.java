c0e9b956ae1056d7fc3f5f7006cb15b811587f92
pramod <pramod.raghavendra.jayathirth@intel.com>
2016-04-05 16:46:55 -0700

    Adds the "Quality of Service" to the OpenDayLight Neutron
    This patch enables the Quality of Service feature in the OpenDayLight
    Neutron.
    The implementation is done as follows
    1. Defining the YANG model
    2. Implementing of API
    3. Testing the developed API
    
    The QOS is achieved using either limiting the bandwidth or through
    DSCP marking
    
    Data model -
    https://specs.openstack.org/openstack/neutron-specs/specs/liberty/qos-api-extension.html
    This link has the data model changes as suggested by the OpenStack
    Neutron for the QOS feature
    API -
    http://developer.openstack.org/api-ref-networking-v2-ext.html#qos-ext
    This link has the API for the Openstack for the QOS feature
    
    Change-Id: I7074dbb6d494b1267b66a925742e9812ef8a9304
    Signed-off-by: pramod <pramod.raghavendra.jayathirth@intel.com>

---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/NeutronQosDscpMarkingRule.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public class NeutronQosDscpMarkingRule extends NeutronObject implements Serializable, INeutronObject {
+    private static final long serialVersionUID = 1L;
+
+    @XmlElement(name = "dscp_mark")
+    Short dscpMark;
+
+    public Short getDscpMark() {
+        return dscpMark;
+    }
+
+    public void setDscpMark(Short dscpMark) {
+        this.dscpMark = dscpMark;
+    }
+
+    public NeutronQosDscpMarkingRule extractFields(List<String> fields) {
+        NeutronQosDscpMarkingRule ans = new NeutronQosDscpMarkingRule();
+        Iterator<String> i = fields.iterator();
+        while (i.hasNext()) {
+            String s = i.next();
+            if (s.equals("id")) {
+                ans.setID(this.getID());
+            }
+            if (s.equals("tenant_id")) {
+                ans.setTenantID(this.getTenantID());
+            }
+            if (s.equals("dscp_mark")) {
+                ans.setDscpMark(this.getDscpMark());
+            }
+        }
+        return ans;
+    }
+
+    @Override
+    public String toString() {
+        return "qosDscpRules{" +
+            "qosDscpRuleUUID='" + uuid + '\'' +
+            ", qosDscpRuleTenantID='" + tenantID + '\'' +
+            ", qosDscpRuleDscpMark='" + dscpMark +
+            '}';
+    }
+}
