00ad9f99b1975586ef2f0275171bbbf5af67293d
Anil Vishnoi <vishnoianil@gmail.com>
2016-06-26 14:48:43 -0700

    OpenDaylight Networking SFC Port Pair REST APIs
    This patch defines REST API's in Neutron Northbound project
    for the networking sfc port pair APIs and implementation of
    the translation code to convert these REST API data to the
    networking-sfc Yang models. These Neutron Northbound REST
    API's are defined based on the OpenStack Networking-SFC Flow
    Classifier APIs available at following URL.
    
    http://docs.openstack.org/developer/networking-sfc/api.html#rest-api
    
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>
    Change-Id: Ie8b3bf725e492443ac4b0056c255a71e16b1635a

---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/NeutronSFCPortPair.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.neutron.spi;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+
+public class NeutronSFCPortPair extends NeutronObject implements Serializable, INeutronObject {
+
+    private static final long serialVersionUID = 1L;
+
+    // See OpenStack Networking SFC (networking-sfc) Port Pair API v1.0 Reference
+    // for description of annotated attributes
+    @XmlElement(name = "name")
+    String name;
+
+    @XmlElement(name = "ingress")
+    String ingressPortUUID;
+
+    @XmlElement(name = "egress")
+    String egressPortUUID;
+
+    @XmlElement(name = "service_function_parameters")
+    @XmlJavaTypeAdapter(NeutronResourceMapPropertyAdapter.class)
+    Map<String, String> serviceFunctionParameters;
+
+    public NeutronSFCPortPair() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngressPortUUID() {
+        return ingressPortUUID;
+    }
+
+    public void setIngressPortUUID(String ingressPortUUID) {
+        this.ingressPortUUID = ingressPortUUID;
+    }
+
+    public String getEgressPortUUID() {
+        return egressPortUUID;
+    }
+
+    public void setEgressPortUUID(String egressPortUUID) {
+        this.egressPortUUID = egressPortUUID;
+    }
+
+    public Map<String, String> getServiceFunctionParameters() {
+        return serviceFunctionParameters;
+    }
+
+    public void setServiceFunctionParameters(Map<String, String> serviceFunctionParameters) {
+        this.serviceFunctionParameters = serviceFunctionParameters;
+    }
+
+    /**
+     * This method copies selected fields from the object and returns them
+     * as a new object, suitable for marshaling.
+     *
+     * @param fields List of attributes to be extracted
+     * @return an OpenStack Neutron SFC Port Pair object with only the selected fields
+     * populated
+     */
+
+    public NeutronSFCPortPair extractFields(List<String> fields) {
+        NeutronSFCPortPair ans = new NeutronSFCPortPair();
+        Iterator<String> i = fields.iterator();
+        while (i.hasNext()) {
+            String s = i.next();
+            if (s.equals("id")) {
+                ans.setID(this.getID());
+            }
+            if (s.equals("tenant_id")) {
+                ans.setTenantID(this.getTenantID());
+            }
+            if (s.equals("name")) {
+                ans.setName(this.getName());
+            }
+            if (s.equals("ingress")) {
+                ans.setIngressPortUUID(this.getIngressPortUUID());
+            }
+            if (s.equals("egress")) {
+                ans.setEgressPortUUID(this.getEgressPortUUID());
+            }
+            if (s.equals("service_function_parameters")) {
+                ans.setServiceFunctionParameters(new HashMap<String, String>(this.getServiceFunctionParameters()));
+            }
+        }
+        return ans;
+    }
+
+
+    @Override
+    public String toString() {
+        return "NeutronSFCPortPair[" +
+                "tenantID='" + tenantID + '\'' +
+                ", name='" + name + '\'' +
+                ", ingressPortUUID='" + ingressPortUUID + '\'' +
+                ", egressPortUUID='" + egressPortUUID + '\'' +
+                ", serviceFunctionParameters=" + serviceFunctionParameters +
+                ']';
+    }
+}
---
+++ transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronSFCPortPairInterface.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.neutron.transcriber;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.neutron.spi.INeutronSFCPortPairCRUD;
+import org.opendaylight.neutron.spi.NeutronSFCPortPair;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.sfc.rev160511.port.pair.attributes.ServiceFunctionParameters;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.sfc.rev160511.port.pair.attributes.ServiceFunctionParametersBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.sfc.rev160511.port.pair.attributes.ServiceFunctionParametersKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.sfc.rev160511.sfc.attributes.PortPairs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.sfc.rev160511.sfc.attributes.port.pairs.PortPair;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.sfc.rev160511.sfc.attributes.port.pairs.PortPairBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Anil Vishnoi (avishnoi@Brocade.com)
+ */
+public class NeutronSFCPortPairInterface
+        extends AbstractNeutronInterface<PortPair, PortPairs, NeutronSFCPortPair>
+        implements INeutronSFCPortPairCRUD {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NeutronSFCPortPairInterface.class);
+
+    NeutronSFCPortPairInterface(DataBroker db) {
+        super(db);
+    }
+
+    @Override
+    protected InstanceIdentifier<PortPair> createInstanceIdentifier(PortPair portPair) {
+        return InstanceIdentifier.create(Neutron.class).child(PortPairs.class)
+                .child(PortPair.class, portPair.getKey());
+    }
+
+    @Override
+    protected InstanceIdentifier<PortPairs> createInstanceIdentifier() {
+        return InstanceIdentifier.create(Neutron.class).child(PortPairs.class);
+    }
+
+    @Override
+    protected PortPair toMd(NeutronSFCPortPair neutronPortPair) {
+
+        LOGGER.trace("toMd: REST SFC Port Pair data : {}", neutronPortPair);
+
+        PortPairBuilder result = new PortPairBuilder();
+        result.setUuid(new Uuid(neutronPortPair.getID()));
+        result.setName(neutronPortPair.getName());
+        result.setTenantId(toUuid(neutronPortPair.getTenantID()));
+        if (neutronPortPair.getIngressPortUUID() != null ) {
+            result.setIngress(new Uuid(neutronPortPair.getIngressPortUUID()));
+        }
+        if (neutronPortPair.getEgressPortUUID() != null) {
+            result.setEgress(new Uuid(neutronPortPair.getEgressPortUUID()));
+        }
+        if (neutronPortPair.getServiceFunctionParameters()!= null) {
+            List<ServiceFunctionParameters> serviceFunctionParams = new ArrayList<>();
+            for(String paramKey : neutronPortPair.getServiceFunctionParameters().keySet()) {
+                ServiceFunctionParametersBuilder param = new ServiceFunctionParametersBuilder();
+                param.setKey(new ServiceFunctionParametersKey(paramKey));
+                param.setServiceFunctionParameter(paramKey);
+                param.setServiceFunctionParameterValue(neutronPortPair.getServiceFunctionParameters().get(paramKey));
+                serviceFunctionParams.add(param.build());
+            }
+            result.setServiceFunctionParameters(serviceFunctionParams);
+        }
+        LOGGER.trace("toMd: Yang SFC Port Pair data : {}", result);
+        return result.build();
+    }
+
+    @Override
+    protected PortPair toMd(String uuid) {
+        final PortPairBuilder portPairBuilder = new PortPairBuilder();
+        portPairBuilder.setUuid(toUuid(uuid));
+        return portPairBuilder.build();
+    }
+
+    @Override
+    protected NeutronSFCPortPair fromMd(PortPair mdPortPair) {
+        LOGGER.trace("fromMd: Yang SFC Port Pair data : {}", mdPortPair);
+        NeutronSFCPortPair result = new NeutronSFCPortPair();
+        result.setID(mdPortPair.getUuid().getValue());
+        result.setName(mdPortPair.getName());
+        result.setTenantID(mdPortPair.getTenantId());
+        if (mdPortPair.getIngress() !=null) {
+            result.setIngressPortUUID(mdPortPair.getIngress().getValue());
+        }
+        if (mdPortPair.getEgress() != null) {
+            result.setEgressPortUUID(mdPortPair.getEgress().getValue());
+        }
+        if (mdPortPair.getServiceFunctionParameters()!= null) {
+            HashMap<String, String> serviceFunctionParam = new HashMap<>();
+            for(ServiceFunctionParameters param : mdPortPair.getServiceFunctionParameters()) {
+                serviceFunctionParam.put(param.getServiceFunctionParameter(),param.getServiceFunctionParameterValue());
+            }
+            result.setServiceFunctionParameters(serviceFunctionParam);
+        }
+        LOGGER.trace("fromMd: REST SFC Port Pair data : {}", result);
+        return result;
+    }
+
+    @Override
+    protected List<PortPair> getDataObjectList(PortPairs dataObjects) {
+        return dataObjects.getPortPair();
+    }
+
+}
