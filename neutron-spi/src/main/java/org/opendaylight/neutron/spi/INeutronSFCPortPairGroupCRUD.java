c13e31f8b26454b31ffae9615913116f3e55f225
Anil Vishnoi <vishnoianil@gmail.com>
2016-06-26 16:10:56 -0700

    OpenDaylight Networking SFC Port Pair Group REST APIs
    This patch defines REST API's in Neutron Northbound project
    for the networking sfc port pair group APIs and implementation of
    the translation code to convert these REST API data to the
    networking-sfc port pair group Yang models. These Neutron
    Northbound REST API's are defined based on the OpenStack
    Networking-SFC Port Pair Group APIs available at following URL.
    
    http://docs.openstack.org/developer/networking-sfc/api.html#rest-api
    
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>
    Change-Id: Ib25544b711873983be7733ec552328220fd8a85d

---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/INeutronSFCPortPairGroupCRUD.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.neutron.spi;
+
+/**
+ * This interface defines the methods for CRUD of NB SFC Port Pair Group objects
+ *
+ */
+public interface INeutronSFCPortPairGroupCRUD extends INeutronCRUD<NeutronSFCPortPairGroup> {
+    // Nothing Here.
+    // This class is defined to use reflection.
+}
---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/NeutronSFCPortPairGroup.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.neutron.spi;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+
+public class NeutronSFCPortPairGroup extends NeutronObject implements Serializable, INeutronObject {
+    private static final long serialVersionUID = 1L;
+
+    // See OpenStack Networking SFC (networking-sfc) Port Pair Group API v1.0
+    // Reference for description of annotated attributes
+
+    @XmlElement(name = "name")
+    String name;
+
+    @XmlElement(name = "port_pairs")
+    List<String> portPairs;
+
+    public NeutronSFCPortPairGroup() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPortPairs() {
+        return portPairs;
+    }
+
+    public void setPortPairs(List<String> portPairs) {
+        this.portPairs = portPairs;
+    }
+
+    /**
+     * This method copies selected fields from the object and returns them
+     * as a new object, suitable for marshaling.
+     *
+     * @param fields List of attributes to be extracted
+     * @return an OpenStack Neutron SFC Port Pair Group object with only the selected fields
+     * populated
+     */
+
+    public NeutronSFCPortPairGroup extractFields(List<String> fields) {
+        NeutronSFCPortPairGroup ans = new NeutronSFCPortPairGroup();
+        Iterator<String> i = fields.iterator();
+        while (i.hasNext()) {
+            String s = i.next();
+            if (s.equals("id")) {
+                ans.setID(this.getID());
+            }
+            if (s.equals("tenant_id")) {
+                ans.setTenantID(this.getTenantID());
+            }
+            if (s.equals("name")) {
+                ans.setName(this.getName());
+            }
+            if (s.equals("port_pairs")) {
+                ans.setPortPairs(this.getPortPairs());
+            }
+        }
+        return ans;
+    }
+
+    @Override
+    public String toString() {
+        return "NeutronSFCPortPairGroup[" +
+                "tenantID='" + tenantID + '\'' +
+                ", name='" + name + '\'' +
+                ", portPairs=" + portPairs +
+                ']';
+    }
+}
