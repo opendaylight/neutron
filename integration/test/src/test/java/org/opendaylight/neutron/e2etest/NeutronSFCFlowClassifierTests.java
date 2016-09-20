b7066c0be50ec0456831f4f844a1f10ab53d0b0f
pramod <pramod.raghavendra.jayathirth@intel.com>
2016-09-19 20:00:06 -0700

    Adds SFC Integration test cases
    Change-Id: Ie6844549f939123687226c3cbde0cd6239a85926
    Signed-off-by: pramod <pramod.raghavendra.jayathirth@intel.com>

---
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronSFCFlowClassifierTests.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2016 Intel, Corp.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.e2etest;
+
+public class NeutronSFCFlowClassifierTests {
+    String base;
+
+    public NeutronSFCFlowClassifierTests(String base) {
+        this.base = base;
+    }
+
+    public void sfc_flowclassifier_collection_get_test() {
+        String url = base + "/sfc/flowclassifiers";
+        ITNeutronE2E.test_fetch(url, "Flowclassifier group collection GET failed");
+    }
+
+    public String singleton_sfc_flowclassifier_create_test() {
+        String url = base + "/sfc/flowclassifiers";
+        String content = "{ \"flowclassifier\" : { \"name\": \"flowclassifier1\", "
+            + "\"ethertype\": \"IPv4\", "
+            + "\"protocol\": \"UDP\", "
+            + "\"source_port_range_min\": 100, "
+            + "\"source_port_range_max\": 200, "
+            + "\"destination_port_range_min\": 100, "
+            + "\"destination_port_range_max\": 200, "
+            + "\"source_ip_prefix\": \"10.0.0.0/24\", "
+            + "\"destination_ip_prefix\": \"11.0.0.0/24\", "
+            + "\"logical_source_port\": \"5e8e5957-649f-477b-9e5b-f1f75b21c03c\", "
+            + "\"logical_destination_port\": \"6e8e5957-649f-477b-9e5b-f1f75b21c03c\", "
+            + "\"l7_parameters\": [ "
+            + "{ "
+            + "\"Key\": \"value\" "
+            + "} "
+            + "], "
+            + "\"tenant_id\": \"4969c491a3c74ee4af974e6d800c62de\", "
+            + "\"id\": \"4e8e5957-649f-477b-9e5b-f1f75b21c03c\" } }";
+        ITNeutronE2E.test_create(url, content, " SFC Flowclassifier group singleton POST failed");
+        return content;
+    }
+
+    public void singleton_sfc_flowclassifier_get_with_query_item(String createJsonString) {
+        String url = base + "/sfc/flowclassifiers";
+        ITNeutronE2E.test_fetch_with_one_query_item(url, createJsonString, "flowclassifiers");
+    }
+
+    public void sfc_flowclassifier_element_get_test() {
+        String url = base + "/sfc/flowclassifiers";
+        ITNeutronE2E.test_fetch(url, true, "SFC flowclassifier elements GET failed");
+    }
+
+    public void sfc_flowclassifier_get_test_with_query_test() {
+        String url = base + "/sfc/flowclassifiers/4e8e5957-649f-477b-9e5b-f1f75b21c03c"
+            + "?fields=name&fields=ethertype&fields=protocol&fields=source_port_range_min"
+            + "&fields=source_port_range_max&fields=destination_port_range_min&fields=destination_port_range_max"
+            + "&fields=source_ip_prefix&fields=destination_ip_prefix&fields=logical_source_port"
+            + "&fields=logical_destination_port&fields=l7_parameters&fields=tenant_id&fields=id";
+        ITNeutronE2E.test_fetch(url, true, "SFC flowclassifier group element GET with query failed");
+    }
+
+    public void sfc_flowclassifier_modify_test() {
+        String url = base + "/sfc/flowclassifiers/4e8e5957-649f-477b-9e5b-f1f75b21c03c";
+        String content = "{ \"flowclassifier\" : { \"name\": \"flowclassifier1\", "
+            + "\"ethertype\": \"IPv4\", "
+            + "\"protocol\": \"UDP\", "
+            + "\"source_port_range_min\": 100, "
+            + "\"source_port_range_max\": 200, "
+            + "\"destination_port_range_min\": 100, "
+            + "\"destination_port_range_max\": 200, "
+            + "\"source_ip_prefix\": \"10.0.0.0/24\", "
+            + "\"destination_ip_prefix\": \"11.0.0.0/24\", "
+            + "\"logical_source_port\": \"5e8e5957-649f-477b-9e5b-f1f75b21c03c\", "
+            + "\"logical_destination_port\": \"6e8e5957-649f-477b-9e5b-f1f75b21c03c\", "
+            + "\"l7_parameters\": [ "
+            + "{ "
+            + "\"Key\": \"value\" "
+            + "} "
+            + "], "
+            + "\"tenant_id\": \"4969c491a3c74ee4af974e6d800c62de\", "
+            + "\"id\": \"4e8e5957-649f-477b-9e5b-f1f75b21c03c\" } }";
+        ITNeutronE2E.test_modify(url, content, "SFC flowclassifier group singleton PUT failed");
+    }
+
+    public void sfc_flowclassifier_delete_test() {
+        String url = base + "/sfc/flowclassifiers/4e8e5957-649f-477b-9e5b-f1f75b21c03c";
+        ITNeutronE2E.test_delete(url, "SFC flowclassifier DELETE failed");
+    }
+
+    public void sfc_flowclassifier_element_negative_get_test() {
+        String url = base + "/sfc/flowclassifiers/4e8e5957-649f-477b-9e5b-f1f75b21c03c";
+        ITNeutronE2E.test_fetch(url, false, "SFC flowclassifier Element negative GET failed");
+    }
+
+    public static void runTests(String base) {
+        NeutronSFCFlowClassifierTests sfc_flowclassifier_tester = new NeutronSFCFlowClassifierTests(base);
+        String createJsonString = sfc_flowclassifier_tester.singleton_sfc_flowclassifier_create_test();
+        sfc_flowclassifier_tester.singleton_sfc_flowclassifier_get_with_query_item(createJsonString);
+        sfc_flowclassifier_tester.sfc_flowclassifier_element_get_test();
+        sfc_flowclassifier_tester.sfc_flowclassifier_get_test_with_query_test();
+        sfc_flowclassifier_tester.sfc_flowclassifier_collection_get_test();
+        sfc_flowclassifier_tester.sfc_flowclassifier_modify_test();
+        sfc_flowclassifier_tester.sfc_flowclassifier_delete_test();
+        sfc_flowclassifier_tester.sfc_flowclassifier_element_negative_get_test();
+    }
+}
---
+++ integration/test/src/test/java/org/opendaylight/neutron/e2etest/NeutronSFCPortPairTests.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2016 Intel, Corp.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.e2etest;
+
+public class NeutronSFCPortPairTests {
+    String base;
+
+    public NeutronSFCPortPairTests(String base) {
+        this.base = base;
+    }
+
+    public void sfc_portpair_collection_get_test() {
+        String url = base + "/sfc/portpairs";
+        ITNeutronE2E.test_fetch(url, "Portpair collection GET failed");
+    }
+
+    public String singleton_sfc_portpair_create_test() {
+        String url = base + "/sfc/portpairs";
+        String content = " { \"portpair\": { "
+            + "\"name\": \"portpair1\", "
+            + "\"ingress\": \"5e8e5957-649f-477b-9e5b-f1f75b21c03c\", "
+            + "\"egress\": \"6e8e5957-649f-477b-9e5b-f1f75b21c03c\", "
+            + "\"service_function_parameters\": [ "
+            + "{ "
+            + "\"correlation\": \"value\" "
+            + "} "
+            + "], "
+            + "\"tenant_id\": \"4969c491a3c74ee4af974e6d800c62de\", "
+            + "\"id\": \"4e8e5957-649f-477b-9e5b-f1f75b21c03c\" } }";
+        ITNeutronE2E.test_create(url, content, "SFC Portpair Singleton POST failed");
+        return content;
+    }
+
+    public void singleton_sfc_portpair_get_with_query_item_test(String createJsonString) {
+        String url = base + "/sfc/portpairs";
+        ITNeutronE2E.test_fetch_with_one_query_item(url, createJsonString, "portpairs");
+    }
+
+    public void sfc_portpair_element_get_test() {
+        String url = base + "/sfc/portpairs/4e8e5957-649f-477b-9e5b-f1f75b21c03c";
+        ITNeutronE2E.test_fetch(url, true, "SFC Portpair Element GET failed");
+    }
+
+    public void sfc_portpair_element_get_with_query_test() {
+        String url = base + "/sfc/portpairs/4e8e5957-649f-477b-9e5b-f1f75b21c03c"
+            + "?fields=name&fields=ingress&fields=egress"
+            + "&fields=limits&fields=marker&fields=page_reverse";
+        ITNeutronE2E.test_fetch(url, true, "SFC Portpair Element GET with query failed");
+    }
+
+    public void sfc_portpair_modify_test() {
+        String url = base + "/sfc/portpairs/4e8e5957-649f-477b-9e5b-f1f75b21c03c";
+        String content = " { \"portpair\": { "
+            + "\"name\": \"portpair2\", "
+            + "\"ingress\": \"5e8e5957-649f-477b-9e5b-f1f75b21c03c\", "
+            + "\"egress\": \"6e8e5957-649f-477b-9e5b-f1f75b21c03c\", "
+            + "\"service_function_parameters\": [ "
+            + "{ "
+            + "\"correlation\": \"value\" "
+            + "} "
+            + "], "
+            + "\"tenant_id\": \"4969c491a3c74ee4af974e6d800c62de\", "
+            + "\"id\": \"4e8e5957-649f-477b-9e5b-f1f75b21c03c\" }}";
+        ITNeutronE2E.test_modify(url, content, "SFC Portpair Singleton PUT failed");
+    }
+
+    public void sfc_portpair_delete_test() {
+        String url = base + "/sfc/portpairs/4e8e5957-649f-477b-9e5b-f1f75b21c03c";
+        ITNeutronE2E.test_delete(url, "SFC Portpair DELETE failed");
+    }
+
+    public void sfc_portpair_element_negative_get_test() {
+        String url = base + "/sfc/portpairs/4e8e5957-649f-477b-9e5b-f1f75b21c03c";
+        ITNeutronE2E.test_fetch(url, false, "SFC Portpair Element Negative GET Failed");
+    }
+
+    public static void runTests(String base) {
+        NeutronSFCPortPairTests sfc_portpair_tester = new NeutronSFCPortPairTests(base);
+        String createJsonString = sfc_portpair_tester.singleton_sfc_portpair_create_test();
+        sfc_portpair_tester.singleton_sfc_portpair_get_with_query_item_test(createJsonString);
+        sfc_portpair_tester.sfc_portpair_element_get_test();
+        sfc_portpair_tester.sfc_portpair_element_get_with_query_test();
+        sfc_portpair_tester.sfc_portpair_collection_get_test();
+        sfc_portpair_tester.sfc_portpair_modify_test();
+        sfc_portpair_tester.sfc_portpair_delete_test();
+        sfc_portpair_tester.sfc_portpair_element_negative_get_test();
+    }
+}
