0335c4d2d9df18389050a18240b11cdfe767744d
Vishal Thapar <vishal.thapar@ericsson.com>
2017-01-14 18:50:42 +0530

    Add support for Openstack Neutron Trunkport APIs
    This adds the following:
    1. Yang model for TrunkPort Openstack Neuton v2 extension
    2. Implementation of API (spi, api and transcriber)
    
    References:
    https://blueprints.launchpad.net/neutron/+spec/vlan-aware-vms
    http://specs.openstack.org/openstack/neutron-specs/specs/newton/vlan-aware-vms.html
    https://wiki.openstack.org/wiki/Neutron/TrunkPort
    
    Pending:
    1. UTs and ITs for the API
    
    Change-Id: I1428e44f93a5bc4507ad5b39c1f2f9f92540fab8
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

---
+++ northbound-api/src/main/java/org/opendaylight/neutron/northbound/api/NeutronTrunkRequest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.northbound.api;
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.opendaylight.neutron.spi.NeutronTrunk;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public final class NeutronTrunkRequest implements INeutronRequest<NeutronTrunk> {
+
+    @XmlElement(name = "trunk")
+    NeutronTrunk singleton;
+
+    @XmlElement(name = "trunks")
+    List<NeutronTrunk> bulkRequest;
+
+    NeutronTrunkRequest() {
+    }
+
+    NeutronTrunkRequest(NeutronTrunk trunk) {
+        singleton = trunk;
+    }
+
+    NeutronTrunkRequest(List<NeutronTrunk> bulk) {
+        bulkRequest = bulk;
+    }
+}
---
+++ transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronTrunkInterface.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.transcriber;
+
+
+import com.google.common.collect.ImmutableBiMap;
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.neutron.spi.INeutronTrunkCRUD;
+import org.opendaylight.neutron.spi.NeutronTrunk;
+import org.opendaylight.neutron.spi.NeutronTrunkSubPort;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.NetworkTypeBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.NetworkTypeFlat;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.NetworkTypeGre;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.NetworkTypeVlan;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.NetworkTypeVxlan;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunk.attributes.SubPorts;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunk.attributes.SubPortsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunks.attributes.Trunks;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunks.attributes.trunks.Trunk;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunks.attributes.trunks.TrunkBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunks.attributes.trunks.TrunkKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class NeutronTrunkInterface extends AbstractNeutronInterface<Trunk, Trunks, TrunkKey, NeutronTrunk>
+        implements INeutronTrunkCRUD {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NeutronTrunkInterface.class);
+    private static final ImmutableBiMap<Class<? extends NetworkTypeBase>,
+    String> NETWORK_TYPE_MAP = new ImmutableBiMap.Builder<Class<? extends NetworkTypeBase>, String>()
+            .put(NetworkTypeFlat.class, "flat").put(NetworkTypeGre.class, "gre")
+            .put(NetworkTypeVlan.class, "vlan").put(NetworkTypeVxlan.class, "vxlan").build();
+
+    NeutronTrunkInterface(DataBroker db) {
+        super(TrunkBuilder.class, db);
+    }
+
+    @Override
+    protected List<Trunk> getDataObjectList(Trunks trunks) {
+        return trunks.getTrunk();
+    }
+
+    @Override
+    protected NeutronTrunk fromMd(Trunk trunk) {
+        final NeutronTrunk result = new NeutronTrunk();
+        fromMdAdminAttributes(trunk, result);
+        if(trunk.getPortId() != null) {
+            result.setPortId(trunk.getPortId().getValue());
+        }
+        if(trunk.getSubPorts() != null) {
+            final List<NeutronTrunkSubPort> subPortList = new ArrayList<>();
+            for(final SubPorts subPort : trunk.getSubPorts()) {
+                NeutronTrunkSubPort trunkSubPort = new NeutronTrunkSubPort();
+                trunkSubPort.setPortId(subPort.getPortId().getValue());
+                trunkSubPort.setSegmentationType(NETWORK_TYPE_MAP.get(subPort.getSegmentationType()));
+                trunkSubPort.setSegmentationId(subPort.getSegmentationId().toString());
+                subPortList.add(trunkSubPort);
+            }
+            result.setSubPorts(subPortList);
+        }
+        return result;
+    }
+
+    @Override
+    protected Trunk toMd(NeutronTrunk trunk) {
+        final TrunkBuilder trunkBuilder = new TrunkBuilder();
+        toMdAdminAttributes(trunk, trunkBuilder);
+        if(trunk.getPortId() != null) {
+            trunkBuilder.setPortId(toUuid(trunk.getPortId()));
+        }
+        if(trunk.getSubPorts() != null) {
+            final List<SubPorts> subPortsList = new ArrayList<>();
+            final SubPortsBuilder subPortsBuilder = new SubPortsBuilder();
+            final ImmutableBiMap<String, Class<? extends NetworkTypeBase>> mapper = NETWORK_TYPE_MAP.inverse();
+            for(NeutronTrunkSubPort subPort: trunk.getSubPorts()) {
+                subPortsBuilder.setPortId(toUuid(subPort.getPortId()));
+                subPortsBuilder.setSegmentationType(mapper.get(subPort.getSegmentationType()));
+                subPortsBuilder.setSegmentationId(Long.valueOf(subPort.getSegmentationId()));
+                subPortsList.add(subPortsBuilder.build());
+            }
+            trunkBuilder.setSubPorts(subPortsList);
+        }
+        return trunkBuilder.build();
+    }
+}
