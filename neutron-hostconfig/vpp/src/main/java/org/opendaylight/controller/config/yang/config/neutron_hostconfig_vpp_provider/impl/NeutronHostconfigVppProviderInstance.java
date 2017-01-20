5e8c85d7c6764219e853363e26a4eb0d0725cc80
Tomas Cechvala <tcechval@cisco.com>
2017-01-20 20:48:39 +0100

    WIP Hostconfig for VPP
    L2 configuration is currently supported for VPP  devices.
    Vhostuser is currently suppored vif type in host configuration.
    
    Change-Id: I78b6ad2eeb3d1b11b09317f4c9be0e6878e71df8
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

---
+++ neutron-hostconfig/vpp/src/main/java/org/opendaylight/controller/config/yang/config/neutron_hostconfig_vpp_provider/impl/NeutronHostconfigVppProviderInstance.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Intel Corporation and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.config.yang.config.neutron_hostconfig_vpp_provider.impl;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.neutron.hostconfig.vpp.NeutronHostconfigVppListener;
+import org.opendaylight.neutron.hostconfig.vpp.SocketInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+
+public class NeutronHostconfigVppProviderInstance implements AutoCloseable {
+
+    private final NeutronHostconfigVppListener neutronHostconfigVppListener;
+    
+    private static final Logger LOG = LoggerFactory.getLogger(NeutronHostconfigVppProviderInstance.class);
+
+    public NeutronHostconfigVppProviderInstance(final DataBroker dataBroker, String socketPath, String socketName) {
+        neutronHostconfigVppListener = new NeutronHostconfigVppListener(Preconditions.checkNotNull(dataBroker), new SocketInfo(socketPath, socketName));
+        LOG.info("Initializing Neutron-Hostconfig-Vpp-Listener");
+    }
+
+    public void initialize() {
+        neutronHostconfigVppListener.start();
+    }
+
+    @Override
+    public void close() throws Exception {
+        neutronHostconfigVppListener.close();
+    }
+
+}
---
+++ neutron-hostconfig/vpp/src/main/java/org/opendaylight/neutron/hostconfig/vpp/SocketInfo.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.hostconfig.vpp;
+
+import com.google.common.base.Preconditions;
+
+public class SocketInfo {
+
+    private final String socketPath;
+    private final String socketPrefix;
+    private final static String PORT_ID = "$PORT_ID";
+
+    public SocketInfo(String socketPath, String socketPrefix) {
+        this.socketPath = Preconditions.checkNotNull(socketPath);
+        this.socketPrefix = Preconditions.checkNotNull(socketPrefix);
+    }
+
+    public String getSocketPath() {
+        return socketPath;
+    }
+
+    public String getSocketPrefix() {
+        return socketPrefix;
+    }
+
+    public String getVhostUserSocket() {
+        return new StringBuilder().append(socketPath).append(socketPrefix).append(PORT_ID).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((socketPath == null) ? 0 : socketPath.hashCode());
+        result = prime * result + ((socketPrefix == null) ? 0 : socketPrefix.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SocketInfo other = (SocketInfo) obj;
+        if (socketPath == null) {
+            if (other.socketPath != null)
+                return false;
+        } else if (!socketPath.equals(other.socketPath))
+            return false;
+        if (socketPrefix == null) {
+            if (other.socketPrefix != null)
+                return false;
+        } else if (!socketPrefix.equals(other.socketPrefix))
+            return false;
+        return true;
+    }
+}
