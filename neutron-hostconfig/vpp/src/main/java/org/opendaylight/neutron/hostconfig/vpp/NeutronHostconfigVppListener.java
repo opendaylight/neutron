c0a86f9af88325f5a5404e82f7b8388dda164d27
Tomas Cechvala <tcechval@cisco.com>
2017-02-13 18:19:30 +0100

    Hostconfig for VPP
    L2 configuration is supported for VPP  devices.
    Vhostuser is supported vif type in host configuration.
    
    Change-Id: I78b6ad2eeb3d1b11b09317f4c9be0e6878e71df8
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

---
+++ neutron-hostconfig/vpp/src/main/java/org/opendaylight/neutron/hostconfig/vpp/NeutronHostconfigVppListener.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2017 Intel Corporation. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.hostconfig.vpp;
+
+import com.google.common.base.Preconditions;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nonnull;
+
+import org.opendaylight.controller.md.sal.binding.api.ClusteredDataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.neutron.hostconfig.utils.NeutronHostconfigUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeConnectionStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.connection.status.available.capabilities.AvailableCapability;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeutronHostconfigVppListener implements ClusteredDataTreeChangeListener<Node> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NeutronHostconfigVppListener.class);
+    private final DataBroker dataBroker;
+    private final NeutronHostconfigUtils neutronHostconfig;
+    private ListenerRegistration<DataTreeChangeListener<Node>> listenerRegistration;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+    private static final TopologyId TOPOLOGY_NETCONF = new TopologyId("topology-netconf");
+    private static final QName V3PO_CAPABILITY = QName.create(URI.create("urn:opendaylight:params:xml:ns:yang:v3po"),
+            NeutronHostconfigUtils.parseDate("2016-12-14"), "v3po");
+    private static final QName INTERFACES_CAPABILITY =
+            QName.create(URI.create("urn:ietf:params:xml:ns:yang:ietf-interfaces"),
+                    NeutronHostconfigUtils.parseDate("2014-05-08"), "ietf-interfaces");
+    private final List<QName> requiredCapabilities;
+    private SocketInfo socketInfo;
+
+    public NeutronHostconfigVppListener(final DataBroker dataBroker, String spath, String sname, String vhostMode) {
+        LOG.info("Initializing Neutron-Hostconfig-Vpp-Listener");
+        this.dataBroker = Preconditions.checkNotNull(dataBroker);
+        vhostMode = Preconditions.checkNotNull(vhostMode).toLowerCase();
+        Preconditions.checkArgument(vhostMode.equals("server") || vhostMode.equals("client"),
+                "Supported values for vhostuser-mode are client and server.");
+        this.socketInfo =
+                new SocketInfo(Preconditions.checkNotNull(spath), Preconditions.checkNotNull(sname), vhostMode);
+        this.neutronHostconfig = new NeutronHostconfigUtils(dataBroker);
+        requiredCapabilities = new ArrayList<>();
+        requiredCapabilities.add(V3PO_CAPABILITY);
+        requiredCapabilities.add(INTERFACES_CAPABILITY);
+    }
+
+    @Override
+    public void onDataTreeChanged(@Nonnull Collection<DataTreeModification<Node>> changes) {
+        LOG.info("onDataTreeChanged: Received Data Tree Changed ...", changes);
+        executorService.submit(() -> {
+            for (DataTreeModification<Node> change : Preconditions.checkNotNull(changes, "Changes may not be null!")) {
+                final InstanceIdentifier<Node> key = change.getRootPath().getRootIdentifier();
+                final DataObjectModification<Node> mod = change.getRootNode();
+                LOG.info("onDataTreeChanged: Received Data Tree Changed Update of Type={} for Key={}",
+                        mod.getModificationType(), key);
+                switch (mod.getModificationType()) {
+                    case SUBTREE_MODIFIED:
+                        if (validateVppNode(mod.getDataAfter())) {
+                            updateHostConfig(mod.getDataAfter(), NeutronHostconfigUtils.Action.UPDATE);
+                        } else {
+                            updateHostConfig(mod.getDataBefore(), NeutronHostconfigUtils.Action.DELETE);
+                        }
+                        break;
+                    case DELETE:
+                        updateHostConfig(mod.getDataBefore(), NeutronHostconfigUtils.Action.DELETE);
+                        break;
+                    case WRITE:
+                        if (validateVppNode(mod.getDataAfter())) {
+                            updateHostConfig(mod.getDataAfter(), NeutronHostconfigUtils.Action.ADD);
+                        }
+                        break;
+                    default:
+                }
+            }
+        });
+    }
+
+    public void init() {
+        LOG.info("Initializing {}", getClass().getSimpleName());
+        DataTreeIdentifier<Node> dataTreeIdentifier = new DataTreeIdentifier<>(LogicalDatastoreType.OPERATIONAL,
+                InstanceIdentifier.builder(NetworkTopology.class)
+                    .child(Topology.class, new TopologyKey(TOPOLOGY_NETCONF))
+                    .child(Node.class)
+                    .build());
+        listenerRegistration =
+                dataBroker.registerDataTreeChangeListener(dataTreeIdentifier, NeutronHostconfigVppListener.this);
+        LOG.info("Registered listener to netconf nodes {}.", dataTreeIdentifier.getRootIdentifier());
+    }
+
+    private void updateHostConfig(Node node, NeutronHostconfigUtils.Action action) {
+        for (Map.Entry<String, String> entry : HostconfigUtil.createHostconfigsDataFor(node.getNodeId(), socketInfo)
+            .entrySet()) {
+            LOG.info("Updating hostconfig for node {}. Action: {}.", node.getKey(), action);
+            neutronHostconfig.updateMdsal(neutronHostconfig.buildHostConfigInfo(node.getNodeId().getValue(),
+                    entry.getKey(), entry.getValue()), action);
+        }
+    }
+
+    private boolean validateVppNode(Node node) {
+        LOG.info("Registering new node {}", node.getNodeId().getValue());
+        NetconfNode netconfNode = node.getAugmentation(NetconfNode.class);
+        if (netconfNode == null) {
+            LOG.warn("Node {} is not a netconf device", node.getNodeId().getValue());
+            return false;
+        }
+        NetconfNodeConnectionStatus.ConnectionStatus connectionStatus = netconfNode.getConnectionStatus();
+        switch (connectionStatus) {
+            case Connecting:
+                LOG.info("Connecting device {} ...", node.getNodeId().getValue());
+                break;
+            case Connected:
+                if (netconfNode.getAvailableCapabilities() == null
+                        || netconfNode.getAvailableCapabilities().getAvailableCapability() == null
+                        || netconfNode.getAvailableCapabilities().getAvailableCapability().isEmpty()) {
+                    LOG.warn("Node {} does not contain any capabilities", node.getNodeId().getValue());
+                    break;
+                }
+                if (!capabilityCheck(netconfNode.getAvailableCapabilities().getAvailableCapability())) {
+                    LOG.warn("Node {} does not contain all capabilities required by vpp-renderer",
+                            node.getNodeId().getValue());
+                    break;
+                }
+                LOG.info("VPP node connected {}", node.getNodeId().getValue());
+                return true;
+            case UnableToConnect:
+                LOG.warn("Unable to connect to node {}.", node.getNodeId().getValue());
+                break;
+            default:
+        }
+        return false;
+    }
+
+    private boolean capabilityCheck(final List<AvailableCapability> capabilities) {
+        final List<String> availableCapabilities =
+                capabilities.stream().map(AvailableCapability::getCapability).collect(Collectors.toList());
+        return requiredCapabilities.stream().map(QName::toString).allMatch(availableCapabilities::contains);
+    }
+
+    public void close() throws Exception {
+        if (listenerRegistration != null) {
+            listenerRegistration.close();
+            LOG.info("HostConfig listener Closed");
+        }
+    }
+}
---
+++ neutron-hostconfig/vpp/src/test/java/org/opendaylight/neutron/hostconfig/vpp/NeutronHostconfigVppListenerTest.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.hostconfig.vpp;
+
+import com.google.common.util.concurrent.SettableFuture;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nonnull;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.binding.api.ClusteredDataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification.ModificationType;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeConnectionStatus.ConnectionStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.connection.status.AvailableCapabilitiesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.connection.status.available.capabilities.AvailableCapability;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.connection.status.available.capabilities.AvailableCapabilityBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.Hostconfigs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.hostconfigs.Hostconfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.hostconfigs.HostconfigKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class NeutronHostconfigVppListenerTest extends HostconfigsDataBrokerTest
+        implements ClusteredDataTreeChangeListener<Hostconfig> {
+
+    private static final String V3PO = "(urn:opendaylight:params:xml:ns:yang:v3po?revision=2016-12-14)v3po";
+    private static final String INTERFACES =
+            "(urn:ietf:params:xml:ns:yang:ietf-interfaces?revision=2014-05-08)ietf-interfaces";
+    private static final NodeId NODE_ID = new NodeId("node1");
+    private static final String SOCKET_PATH = "/tmp";
+    private static final String SOCKET_PREFIX = "socket_";
+    private static final String VHOSTUSER_MODE = "server";
+    private SettableFuture<Integer> sf;
+    private ListenerRegistration<DataTreeChangeListener<Hostconfig>> listenerRegistration;
+    private NeutronHostconfigVppListener neutronHostconfigVppListener;
+
+    @Before
+    public void init() throws InterruptedException, ExecutionException {
+        DataTreeIdentifier<Hostconfig> dataTreeIdentifier = new DataTreeIdentifier<>(LogicalDatastoreType.OPERATIONAL,
+                hostConfigIid(NODE_ID).firstIdentifierOf(Hostconfigs.class).builder().child(Hostconfig.class).build());
+        listenerRegistration = getDataBroker().registerDataTreeChangeListener(dataTreeIdentifier,
+                NeutronHostconfigVppListenerTest.this);
+        neutronHostconfigVppListener =
+                new NeutronHostconfigVppListener(getDataBroker(), SOCKET_PATH, SOCKET_PREFIX, VHOSTUSER_MODE);
+        neutronHostconfigVppListener.init();
+        sf = SettableFuture.create();
+    }
+
+    @Test
+    public void testPutCreateParentsSuccess() throws Exception {
+        InstanceIdentifier<Node> iid = InstanceIdentifier.builder(NetworkTopology.class)
+            .child(Topology.class, new TopologyKey(new TopologyId("topology-netconf")))
+            .child(Node.class, new NodeKey(new NodeId(NODE_ID)))
+            .build();
+        Node node1 = createNetconfNode(NODE_ID, V3PO, INTERFACES);
+        WriteTransaction writeTx = getDataBroker().newWriteOnlyTransaction();
+        writeTx.put(LogicalDatastoreType.OPERATIONAL, iid, node1, true);
+        writeTx.submit().get();
+        Assert.assertEquals(sf.get(), Integer.valueOf(1));
+        sf = SettableFuture.create();
+        writeTx = getDataBroker().newWriteOnlyTransaction();
+        writeTx.delete(LogicalDatastoreType.OPERATIONAL, iid);
+        writeTx.submit().get();
+        Assert.assertEquals(sf.get(), Integer.valueOf(2));
+    }
+
+    private InstanceIdentifier<Hostconfig> hostConfigIid(@Nonnull NodeId nodeId) {
+        return InstanceIdentifier.builder(Neutron.class)
+            .child(Hostconfigs.class)
+            .child(Hostconfig.class, new HostconfigKey(nodeId.getValue(), HostconfigUtil.L2_HOST_TYPE))
+            .build();
+    }
+
+    private Node createNetconfNode(NodeId nodeId, String... capabilities) {
+        List<AvailableCapability> caps = Arrays.asList(capabilities)
+            .stream()
+            .map(name -> new AvailableCapabilityBuilder().setCapability(name).build())
+            .collect(Collectors.toList());
+        NetconfNode netconfNode = new NetconfNodeBuilder().setConnectionStatus(ConnectionStatus.Connected)
+            .setAvailableCapabilities(new AvailableCapabilitiesBuilder().setAvailableCapability(caps).build())
+            .build();
+        return new NodeBuilder().setNodeId(nodeId).addAugmentation(NetconfNode.class, netconfNode).build();
+    }
+
+    @Override
+    public void onDataTreeChanged(Collection<DataTreeModification<Hostconfig>> hostConfigDtm) {
+        for (DataTreeModification<Hostconfig> dtm : hostConfigDtm) {
+            ModificationType mod = dtm.getRootNode().getModificationType();
+            switch (mod) {
+                case WRITE: {
+                    sf.set(1);
+                    break;
+                }
+                case DELETE: {
+                    sf.set(2);
+                    break;
+                }
+                default:
+            }
+        }
+    }
+
+    @After
+    public void close() throws Exception {
+        if (listenerRegistration != null) {
+            listenerRegistration = null;
+        }
+        if (neutronHostconfigVppListener != null) {
+            neutronHostconfigVppListener.close();
+        }
+    }
+}
