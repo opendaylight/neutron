630068ba7c84f340c38e69e49546b6ded6b24f95
Isaku Yamahata <isaku.yamahata@intel.com>
2017-03-08 11:25:42 -0800

    WIP tapaas: tapaas support Added Tap-As-A-Service implementation in Neutron. Also added postman script to test the implementation.
    Change-Id: I2febd008cfa86e70557123e82a1621eaa80e832c
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

---
+++ northbound-api/src/main/java/org/opendaylight/neutron/northbound/api/NeutronTapFlowRequest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Intel, Corp. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.northbound.api;
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.opendaylight.neutron.spi.NeutronTapFlow;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public final class NeutronTapFlowRequest implements INeutronRequest<NeutronTapFlow> {
+    @XmlElement(name = "tapflow")
+    NeutronTapFlow singleton;
+
+    @XmlElement(name = "tapflows")
+    List<NeutronTapFlow> bulkRequest;
+
+    NeutronTapFlowRequest() {
+    }
+
+    NeutronTapFlowRequest(List<NeutronTapFlow> bulk) {
+        bulkRequest = bulk;
+    }
+
+    NeutronTapFlowRequest(NeutronTapFlow tf) {
+        singleton = tf;
+    }
+}
