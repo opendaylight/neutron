b4fb1e13e50fb3f70ae8f1dd691023bc60cdf7c7
Isaku Yamahata <isaku.yamahata@intel.com>
2017-03-16 22:50:00 -0700

    WIP: l2gateway: fix yang model and transacriber
    For Nitrogen
    remove redundant definition.
    
    Change-Id: I983a66b8839ad23db18dd368c8dea37648d1b7e1
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronL2gatewayInterface.java
+++ transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronL2gatewayInterface.java
@@ -44,16 +44,7 @@ public final class NeutronL2gatewayInterface
     protected NeutronL2gateway fromMd(L2gateway l2gateway) {
         final NeutronL2gateway result = new NeutronL2gateway();
         final List<NeutronL2gatewayDevice> neutronL2gatewayDevices = new ArrayList<NeutronL2gatewayDevice>();
-
-        if (l2gateway.getUuid() != null) {
-            result.setID(l2gateway.getUuid().getValue());
-        }
-        if (l2gateway.getL2gatewayName() != null) {
-            result.setName(String.valueOf(l2gateway.getL2gatewayName()));
-        }
-        if (l2gateway.getTenantId() != null) {
-            result.setTenantID(l2gateway.getTenantId());
-        }
+        fromMdBaseAttributes(l2gateway, result);
         if (l2gateway.getDevices() != null) {
             for (final Devices device : l2gateway.getDevices()) {
                 final NeutronL2gatewayDevice neutronL2gatewayDevice = new NeutronL2gatewayDevice();
@@ -95,16 +86,7 @@ public final class NeutronL2gatewayInterface
     @Override
     protected L2gateway toMd(NeutronL2gateway neutronObject) {
         final L2gatewayBuilder l2gatewayBuilder = new L2gatewayBuilder();
-        if (neutronObject.getName() != null) {
-            l2gatewayBuilder.setL2gatewayName(neutronObject.getName());
-        }
-        if (neutronObject.getID() != null) {
-            l2gatewayBuilder.setUuid(toUuid(neutronObject.getID()));
-        }
-        if (neutronObject.getTenantID() != null) {
-            l2gatewayBuilder.setTenantId(toUuid(neutronObject.getTenantID()));
-        }
-
+        toMdBaseAttributes(neutronObject, l2gatewayBuilder);
         if (neutronObject.getNeutronL2gatewayDevices() != null) {
             final List<Devices> devices = new ArrayList<>();
             for (final NeutronL2gatewayDevice neutronL2gatewayDevice : neutronObject.getNeutronL2gatewayDevices()) {
