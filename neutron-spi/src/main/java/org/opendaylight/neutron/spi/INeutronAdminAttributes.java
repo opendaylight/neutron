41d3dc2f4e9925d7ed1a28de14e5849a285fcf18
mcauffiez <mcauffiez@inocybe.ca>
2017-05-04 10:45:07 -0400

    test
    Change-Id: Ie8d50cef184963dcf19d5085816eddcfb9d4e1bb
    Signed-off-by: mcauffiez <mcauffiez@inocybe.ca>

--- neutron-spi/src/main/java/org/opendaylight/neutron/spi/INeutronAdminAttributes.java
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/INeutronAdminAttributes.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 Intel Coporporation  All rights reserved.
- * Copyright (c) 2016 Isaku Yamahata  All rights reserved.
+ * Copyright (c) 2016 Isaku Yamahata  All rights reserveds.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
