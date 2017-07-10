da694dc3efe4758c2c49bd04b8a8fb7d28bdc5d9
Pramod <pramod.raghavendra.jayathirth@intel.com>
2017-07-10 15:36:19 -0700

    Adds Minimum Bandwidth Rule to the Qos Policy
    Qos Feature on Neutron Northbound has supported
    two rules initially. This patch facilitates the
    addition of a third rule (minimim bandwidth rule)
    
    https://specs.openstack.org/openstack/neutron-specs/specs/newton/ml2-qos-minimum-egress-bw-support.html
    
    Change-Id: Ia3681a28e01c886f6e4ab84d16cf3204e0dad709
    Signed-off-by: Pramod <pramod.raghavendra.jayathirth@intel.com>

---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/NeutronQosMinimumBandwidthRule.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017 Intel Corporation.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.spi;
+import java.io.Serializable;
+import java.math.BigInteger;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public final class NeutronQosMinimumBandwidthRule extends NeutronObject<NeutronQosMinimumBandwidthRule>
+    implements Serializable, INeutronObject<NeutronQosMinimumBandwidthRule> {
+    private static final long serialVersionUID = 1L;
+
+    @XmlElement(name = "min_kbps")
+    BigInteger minKbps;
+
+    @XmlElement(defaultValue = "egress", name = "direction")
+    String direction;
+
+    public BigInteger getMinKbps() {
+        return minKbps;
+    }
+
+    public void setMinKbps(BigInteger minKbps) {
+        this.minKbps = minKbps;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public boolean extractField(String field, NeutronQosMinimumBandwidthRule ans) {
+        switch (field) {
+            case "min_kbps":
+                ans.setMinKbps(this.getMinKbps());
+                break;
+            case "direction":
+                ans.setDirection(this.getDirection());
+                break;
+            default:
+                return super.extractField(field, ans);
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "qosMinimumBandwidthRules{" + "qosMinimumBandwidthRuleUUID='" + uuid + '\'' + ","
+                + " qosMinimumBandwidthRuleTenantID='" + tenantID
+                + '\'' + ", qosMinimumBandwidthMinValue='" + minKbps
+                + '\'' + ", qosMinimumBandwidthDirection='" + direction + '\''
+                + '}';
+    }
+}
