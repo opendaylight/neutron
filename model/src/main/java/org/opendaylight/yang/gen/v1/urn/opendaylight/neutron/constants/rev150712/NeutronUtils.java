38fc8030ed65fac99dcc5acb2f08010a3a9d9d30
Pramod <pramod.raghavendra.jayathirth@intel.com>
2017-07-10 15:36:19 -0700

    Adds Minimum Bandwidth Rule to the Qos Policy
    Qos Feature on Neutron Northbound has supported
    two rules initially. This patch facilitates the
    addition of a third rule (minimim bandwidth rule)
    
    https://specs.openstack.org/openstack/neutron-specs/specs/newton/ml2-qos-minimum-egress-bw-support.html
    
    Change-Id: Ia3681a28e01c886f6e4ab84d16cf3204e0dad709
    Signed-off-by: Pramod <pramod.raghavendra.jayathirth@intel.com>

--- model/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/neutron/constants/rev150712/NeutronUtils.java
+++ model/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/neutron/constants/rev150712/NeutronUtils.java
@@ -62,4 +62,40 @@ public final class NeutronUtils {
             return inverseMapper.get(key);
         }
     }
+
+    // Direction of the Traffic
+    public static class DirectionMapper {
+        private DirectionMapper() {
+            throw new UnsupportedOperationException("Utility class should not be instantiated.");
+        }
+
+        private static final ImmutableBiMap<String, Class<? extends DirectionBase>> MAPPER
+            = new ImmutableBiMap.Builder<String, Class<? extends DirectionBase>>()
+            .put("egress", DirectionEgress.class)
+            .put("ingress", DirectionIngress.class)
+            .build();
+
+        public static Class<? extends DirectionBase> get(String key) {
+            return MAPPER.get(key);
+        }
+
+        public static String getName(Class<? extends DirectionBase> key) {
+            ImmutableBiMap<Class<? extends DirectionBase>, String> inverseMapper = MAPPER.inverse();
+            return inverseMapper.get(key);
+        }
+
+        private static final ImmutableBiMap<String, Class<? extends DirectionEgress>> EGRESSMAPPER
+            = new ImmutableBiMap.Builder<String, Class<? extends DirectionEgress>>()
+            .put("egress", DirectionEgress.class)
+            .build();
+
+        public static Class<? extends DirectionEgress> getEgress(String egresskey) {
+            return EGRESSMAPPER.get(egresskey);
+        }
+
+        public static String getEgressString(Class<? extends DirectionEgress> egresskey) {
+            ImmutableBiMap<Class<? extends DirectionEgress>, String> inverseEgressMapper = EGRESSMAPPER.inverse();
+            return inverseEgressMapper.get(egresskey);
+        }
+    }
 }
