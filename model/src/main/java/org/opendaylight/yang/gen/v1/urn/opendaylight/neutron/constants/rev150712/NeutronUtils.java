599daf76574116904389d82f577e72f458db7c37
Pramod <pramod.raghavendra.jayathirth@intel.com>
2017-07-10 15:36:19 -0700

    Adds Minimum Bandwidth Rule to the Qos Policy
    Qos Feature on Neutron Northbound has supported
    two rules initially. This patch facilitates the
    addition of a third rule (minimim bandwidth rule)
    
    https://specs.openstack.org/openstack/neutron-specs/specs/newton/ml2-qos-minimum-egress-bw-support.html
    
    Change-Id: Ia3681a28e01c886f6e4ab84d16cf3204e0dad709
    Signed-off-by: Pramod <pramod.raghavendra.jayathirth@intel.com>

--- model/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/neutron/constants/rev150712/NeutronUtils.java
+++ model/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/neutron/constants/rev150712/NeutronUtils.java
@@ -10,56 +10,76 @@ package org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.constants.rev15071
 
 import com.google.common.collect.ImmutableBiMap;
 
+import javax.annotation.Nullable;
+
 
 public final class NeutronUtils {
-    private NeutronUtils() {
-        throw new UnsupportedOperationException("Utility class should not be instantiated.");
+    private NeutronUtils () {
+        throw new UnsupportedOperationException ("Utility class should not be instantiated.");
     }
 
     // For FWaaS
     public static class FwProtocolMapper {
-        private FwProtocolMapper() {
-            throw new UnsupportedOperationException("Utility class should not be instantiated.");
+        private FwProtocolMapper () {
+            throw new UnsupportedOperationException ("Utility class should not be instantiated.");
         }
 
-        private static final ImmutableBiMap<String, Class<? extends FwProtocolBase>> MAPPER
-            = new ImmutableBiMap.Builder<String, Class<? extends FwProtocolBase>>()
-            .put("tcp", FwProtocolTcp.class)
-            .put("udp", FwProtocolUdp.class)
-            .put("icmp", FwProtocolIcmp.class)
-            .build();
+        private static final ImmutableBiMap<String, Class<? extends FwProtocolBase>> MAPPER =
+            new ImmutableBiMap.Builder<String, Class<? extends FwProtocolBase>> ()
+                .put ("tcp", FwProtocolTcp.class)
+                .put ("udp", FwProtocolUdp.class)
+                .put ("icmp", FwProtocolIcmp.class).build ();
 
-        public static Class<? extends FwProtocolBase> get(String key) {
-            return MAPPER.get(key);
+        public static Class<? extends FwProtocolBase> get (String key) {
+            return MAPPER.get (key);
         }
 
-        public static String getName(Class<? extends FwProtocolBase> key) {
-            ImmutableBiMap<Class<? extends FwProtocolBase>, String> inverseMapper = MAPPER.inverse();
-            return inverseMapper.get(key);
+        public static String getName (Class<? extends FwProtocolBase> key) {
+            ImmutableBiMap<Class<? extends FwProtocolBase>, String> inverseMapper = MAPPER.inverse ();
+            return inverseMapper.get (key);
         }
     }
 
     // For security group
     public static class ProtocolMapper {
-        private ProtocolMapper() {
-            throw new UnsupportedOperationException("Utility class should not be instantiated.");
+        private ProtocolMapper () {
+            throw new UnsupportedOperationException ("Utility class should not be instantiated.");
         }
 
-        private static final ImmutableBiMap<String, Class<? extends ProtocolBase>> MAPPER
-            = new ImmutableBiMap.Builder<String, Class<? extends ProtocolBase>>()
-            .put("icmp", ProtocolIcmp.class)
-            .put("tcp", ProtocolTcp.class)
-            .put("udp", ProtocolUdp.class)
-            .put("icmpv6", ProtocolIcmpV6.class)
-            .build();
+        private static final ImmutableBiMap<String, Class<? extends ProtocolBase>> MAPPER =
+            new ImmutableBiMap.Builder<String, Class<? extends ProtocolBase>> ()
+                .put ("icmp", ProtocolIcmp.class)
+                .put ("tcp", ProtocolTcp.class).put ("udp", ProtocolUdp.class)
+                .put ("icmpv6", ProtocolIcmpV6.class).build ();
 
-        public static Class<? extends ProtocolBase> get(String key) {
-            return MAPPER.get(key);
+        public static Class<? extends ProtocolBase> get (String key) {
+            return MAPPER.get (key);
         }
 
-        public static String getName(Class<? extends ProtocolBase> key) {
-            ImmutableBiMap<Class<? extends ProtocolBase>, String> inverseMapper = MAPPER.inverse();
-            return inverseMapper.get(key);
+        public static String getName (Class<? extends ProtocolBase> key) {
+            ImmutableBiMap<Class<? extends ProtocolBase>, String> inverseMapper = MAPPER.inverse ();
+            return inverseMapper.get (key);
+        }
+    }
+
+    // For Qos 
+    public static class DirectionMapper {
+        private DirectionMapper () {
+            throw new UnsupportedOperationException ("Utility class should not be instantiated.");
+        }
+
+        private static final ImmutableBiMap<String, Class<? extends DirectionBase>> MAPPER = 
+            new ImmutableBiMap.Builder<String, Class<? extends DirectionBase>> ()
+                .put ("egress", DirectionEgress.class)
+                .put ("ingress", DirectionIngress.class).build ();
+
+        public static Class<? extends DirectionBase> get (String key) {
+            return MAPPER.get (key);
+        }
+
+        public static String getName (Class<? extends DirectionBase> key) {
+            ImmutableBiMap<Class<? extends DirectionBase>, String> inverseMapper = MAPPER.inverse ();
+            return inverseMapper.get (key);
         }
     }
 }
