fec4d4884b576bf0b4cac4f0faf058017f635ff4
Robert Varga <robert.varga@pantheon.tech>
2019-03-14 17:36:59 +0100

    Convert to use MD-SAL APIs
    This mass-converts all of neutron to use MD-SAL APIs.
    
    Change-Id: I4ea16c5a4b506fd6ae4be4cfb46864604584d4f1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronLoadBalancerListenerInterface.java
+++ transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronLoadBalancerListenerInterface.java
@@ -13,7 +13,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.apache.aries.blueprint.annotation.service.Service;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.neutron.northbound.api.BadRequestException;
 import org.opendaylight.neutron.spi.INeutronLoadBalancerListenerCRUD;
 import org.opendaylight.neutron.spi.NeutronID;
