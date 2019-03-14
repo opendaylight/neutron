5c66d3d81facbe9dbd908da4d1a6285a27ce2f6a
Robert Varga <robert.varga@pantheon.tech>
2019-03-14 17:36:59 +0100

    Convert to use MD-SAL APIs
    This mass-converts all of neutron to use MD-SAL APIs.
    
    JIRA: NEUTRON-206
    Change-Id: I4ea16c5a4b506fd6ae4be4cfb46864604584d4f1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Achuth <achuth.maniyedath@gmail.com>

--- transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronFirewallRuleInterface.java
+++ transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronFirewallRuleInterface.java
@@ -12,7 +12,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.apache.aries.blueprint.annotation.service.Service;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.neutron.northbound.api.BadRequestException;
 import org.opendaylight.neutron.spi.INeutronFirewallRuleCRUD;
 import org.opendaylight.neutron.spi.NeutronFirewallRule;
