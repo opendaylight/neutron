cc8ea412fc754c599c226d68d9220a281861bb0a
Robert Varga <robert.varga@pantheon.tech>
2019-03-14 17:36:59 +0100

    Convert to use MD-SAL APIs
    This mass-converts all of neutron to use MD-SAL APIs.
    
    JIRA: NEUTRON-206
    Change-Id: I4ea16c5a4b506fd6ae4be4cfb46864604584d4f1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Achuth <achuth.maniyedath@gmail.com>

--- transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronBgpvpnInterface.java
+++ transcriber/src/main/java/org/opendaylight/neutron/transcriber/NeutronBgpvpnInterface.java
@@ -13,7 +13,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.apache.aries.blueprint.annotation.service.Service;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.neutron.spi.INeutronBgpvpnCRUD;
 import org.opendaylight.neutron.spi.NeutronBgpvpn;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
