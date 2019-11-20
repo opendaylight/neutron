08303a62b18ee74df48329a01c39dd7f8c9b5484
Yenuganti Vasudha <yenuganti.vasudha@ericsson.com>
2019-11-20 16:14:10 +0530

    Adding neutron-filter module
    Signed-off-by: Yenuganti Vasudha <yenuganti.vasudha@ericsson.com>
    Change-Id: Ie221206a43962426001766aae25d44525e6bb5e5

---
+++ neutron-filter/src/main/java/org/opendaylight/neutron/filter/logging/entity/LoggingRequest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2020 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.filter.logging.entity;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class LoggingRequest implements Serializable {
+
+    private static final long serialVersionUID = -4702574169916528738L;
+
+    private String sender;
+
+    private String method;
+
+    private String path;
+
+    private Map<String, String> params;
+
+    private Map<String, String> headers;
+
+    private int contentLength;
+
+    private String body;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getContentLength() {
+        return contentLength;
+    }
+
+    public void setContentLength(int contentLength) {
+        this.contentLength = contentLength;
+    }
+}
+
