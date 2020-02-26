42c44621c6a18b1f34c9a546e3c7ff1f4bb84a86
manjunathpayala <manjupayala@gmail.com>
2020-02-26 18:52:02 +0530

    BGPVPN network and router association
    + Currently, BGPVPN network and router associations are handled by
      BGPVPN update request.
    + Neutron model is changed to support bgpvpn network and router
      association as CREATE/DELETE calls instead of BGPVPN update.
    
    Change-Id: I6de1be538ef1d1e34d8d0e7f20786b6fae7b99b0
    Signed-off-by: manjunathpayala <manjupayala@gmail.com>
    Signed-off-by: Somashekar Byrappa <somashekar.b@altencalsoftlabs.com>

---
+++ northbound-api/src/main/java/org/opendaylight/neutron/northbound/api/NeutronBgpvpnNetworkAssociationRequest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.neutron.northbound.api;
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.opendaylight.neutron.spi.NeutronBgpvpnNetworkAssociation;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public class NeutronBgpvpnNetworkAssociationRequest implements INeutronRequest<NeutronBgpvpnNetworkAssociation> {
+
+    @XmlElement(name = "bgpvpn_network_association")
+    NeutronBgpvpnNetworkAssociation singleton;
+
+    @XmlElement(name = "bgpvpn_network_associations")
+    List<NeutronBgpvpnNetworkAssociation> bulkRequest;
+
+    NeutronBgpvpnNetworkAssociationRequest() {
+    }
+
+    @edu.umd.cs.findbugs.annotations.SuppressWarnings("URF_UNREAD_FIELD")
+    NeutronBgpvpnNetworkAssociationRequest(NeutronBgpvpnNetworkAssociation bgpvpnNetworkAssociation) {
+        this.singleton = bgpvpnNetworkAssociation;
+    }
+
+    @edu.umd.cs.findbugs.annotations.SuppressWarnings("URF_UNREAD_FIELD")
+    NeutronBgpvpnNetworkAssociationRequest(List<NeutronBgpvpnNetworkAssociation> bulk) {
+        this.bulkRequest = bulk;
+    }
+}
