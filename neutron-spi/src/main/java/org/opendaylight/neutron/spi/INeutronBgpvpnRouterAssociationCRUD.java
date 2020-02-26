6c507bb0503a0a91b99cc375ae071da208c382dd
manjunathpayala <manjupayala@gmail.com>
2020-02-26 18:52:02 +0530

    BGPVPN network and router association
    + Currently, BGPVPN network and router associations are handled by
      BGPVPN update request.
    + Neutron model is changed to support bgpvpn network and router
      association as CREATE/DELETE calls instead of BGPVPN update.
    
    Change-Id: I6de1be538ef1d1e34d8d0e7f20786b6fae7b99b0
    Signed-off-by: manjunathpayala <manjupayala@gmail.com>
    Signed-off-by: Somashekar Byrappa <somashekar.b@altencalsoftlabs.com>

---
+++ neutron-spi/src/main/java/org/opendaylight/neutron/spi/INeutronBgpvpnRouterAssociationCRUD.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+
+package org.opendaylight.neutron.spi;
+
+public interface INeutronBgpvpnRouterAssociationCRUD extends INeutronCRUD<NeutronBgpvpnRouterAssociation> {
+    // Nothing Here.
+    // This class is defined to use reflection like INeutronBgpvpnRouterAssociationCRUD.class
+}
